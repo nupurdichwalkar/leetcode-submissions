e98bfecf390da23f42cb3774f5265f0740366530
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2018-11-08 23:32:25 +0000

    [LeetCode Sync] Runtime - 23 ms (11.32%), Memory - N/A (N/A)

---
+++ all-submissions/0561-array-partition/solution.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public int arrayPairSum(int[] nums) {
+    Arrays.sort(nums);
+    int sum = 0;
+    for(int i = 0; i<nums.length; i = i + 2)
+    {
+      sum = sum + nums[i];    
+    }
+    return sum;
+    }
+}
