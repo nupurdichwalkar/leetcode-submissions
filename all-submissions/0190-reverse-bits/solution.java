5a71aa0f7343c8002c97e2a2f974f6323cd6ec5f
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-02-09 20:13:52 +0000

    [LeetCode Sync] Runtime - 1 ms (60.28%), Memory - 14.5 MB (100.00%)

---
+++ all-submissions/0190-reverse-bits/solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+     int mask = 1;
+        int s = n, c, res = 0;
+        for (int i = 0; i < 32; i++) {
+            c = s & mask;
+            res <<= 1;
+            res += c;
+            s >>= 1;
+        }
+        return res;
+    }
+}
