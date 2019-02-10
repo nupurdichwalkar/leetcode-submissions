6dc6eb3f058f45fea58080da5382e47f5d338bd5
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-02-10 20:31:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 27.5 MB (100.00%)

--- all-submissions/0141-linked-list-cycle/solution.java
+++ all-submissions/0141-linked-list-cycle/solution.java
@@ -11,17 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-     if(head == null)
-         return false;
-     ListNode p1 = head;
-        ListNode p2 = head;
-     while(p1.next!=null && p1.next.next!=null && p2.next!=null)
-     {
-         p1 = p1.next.next;
-         p2 = p2.next;
-         if(p1==p2)
-             return true;
-     }
-        return false;
+        if(head==null || head.next==null) 
+            return false;
+        ListNode p1=head.next;
+        ListNode p2=head;
+        
+        while(p1!=p2)
+        {
+            if(p1==null||p1.next==null)
+                return false;
+            p1 = p1.next.next;
+            p2 = p2.next;
+        }
+        return true;
+        
     }
 }
