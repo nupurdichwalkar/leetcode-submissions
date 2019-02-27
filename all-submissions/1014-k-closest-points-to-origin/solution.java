0c2998e8ba1f703d3c232de0ed4747da45262ca7
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-02-27 16:05:00 +0000

    [LeetCode Sync] Runtime - 58 ms (8.94%), Memory - 64.8 MB (5.36%)

--- all-submissions/1014-k-closest-points-to-origin/solution.java
+++ all-submissions/1014-k-closest-points-to-origin/solution.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[][] kClosest(int[][] points, int K) {
-        int[] temp = new int[points.length];
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        int[][] ans = new int[K][2];
-        for(int i = 0; i< points.length; i++)
-        {
-            int dist = points[i][0]*points[i][0] + points[i][1]*points[i][1];
-            map.put(dist,i);
-            temp[i] = dist;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        for (int[] p : points) {
+            pq.offer(p);
+            if (pq.size() > K) {
+                pq.poll();
+            }
         }
-        Arrays.sort(temp);
+        int[][] ans = new int[K][2];
         for(int j = 0; j<K; j++)
         {
-            ans[j][0] = points[map.get(temp[j])][0];
-            ans[j][1] = points[map.get(temp[j])][1];
+            ans[j] = pq.poll();
         }
         return ans; 
     }
