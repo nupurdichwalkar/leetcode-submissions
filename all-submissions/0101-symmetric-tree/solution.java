1f2f708f9d5abd020057e991a99e53e807196cd5
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-03-02 17:57:04 +0000

    [LeetCode Sync] Runtime - 5 ms (0.48%), Memory - 40.4 MB (100.00%)

--- all-submissions/0101-symmetric-tree/solution.java
+++ all-submissions/0101-symmetric-tree/solution.java
@@ -13,9 +13,10 @@ class Solution {
     }
     public boolean isMirror(TreeNode r1, TreeNode r2)
     {
-        if(r1==null&&r2==null) return true;
-        if(r1==null||r2==null) return false;
-        return (r1.val == r2.val) && isMirror(r1.right, r2.left)
-        && isMirror(r1.left, r2.right);
+        if(r1==null && r2==null) 
+            return true;
+        if(r1==null || r2==null) 
+            return false;
+        return (r1.val == r2.val) && isMirror(r1.right, r2.left) && isMirror(r1.left, r2.right);
     }
 }
