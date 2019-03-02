102730aec042a96596bfbd5eb98502b644b33d27
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-03-02 15:43:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 37.1 MB (100.00%)

---
+++ all-submissions/0144-binary-tree-preorder-traversal/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root==null)
+        {
+            return list;
+        }
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));   
+        list.addAll(preorderTraversal(root.right));
+        return list;
+        
+    }
+}
