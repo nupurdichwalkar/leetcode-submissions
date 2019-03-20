4e6f4db6eed3721ef73346fde6a339ecf3fd06e3
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-03-20 18:16:49 +0000

    [LeetCode Sync] Runtime - 39 ms (5.28%), Memory - 41.3 MB (100.00%)

--- all-submissions/0013-roman-to-integer/solution.java
+++ all-submissions/0013-roman-to-integer/solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int romanToInt(String s) {
         HashMap<Character,Integer> RomanTable = new HashMap<Character,Integer>();
-        HashMap<String,Integer> SubTable = new HashMap<String,Integer>();
+        //HashMap<String,Integer> SubTable = new HashMap<String,Integer>();
         int sum = 0, temp =0;
         RomanTable.put('I',1);
         RomanTable.put('V',5);
@@ -10,8 +10,6 @@ class Solution {
         RomanTable.put('C',100);
         RomanTable.put('D',500);
         RomanTable.put('M',1000);
-        RomanTable.put('I',1);
-        RomanTable.put('I',1);
         for (int i=0;i<s.length();i++)
         {
             if((i!=s.length()-1)&&(RomanTable.get(s.charAt(i+1))>RomanTable.get(s.charAt(i))))
