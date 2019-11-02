fe665a38aa79793513ee25195dbb6bca3c3ba5e3
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-11-02 23:27:32 +0000

    [LeetCode Sync] Runtime - 40 ms (29.55%), Memory - 37.6 MB (100.00%)

--- all-submissions/0005-longest-palindromic-substring/solution.java
+++ all-submissions/0005-longest-palindromic-substring/solution.java
@@ -1,20 +1,25 @@
 class Solution {
-   public String longestPalindrome(String s) {
-  int n = s.length();
-  String res = "";
-    
-  boolean[][] dp = new boolean[n][n];
-    
-  for (int i = n - 1; i >= 0; i--) {
-    for (int j = i; j < n; j++) {
-      dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
-            
-      if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
-        res = s.substring(i, j + 1);
-      }
+    public String longestPalindrome(String s) {
+        if(s==null || s.length()<2){
+            return s;
+        }
+        int left = 0;
+        int right = 0;
+        boolean[][] isPalindrome = new boolean[s.length()][s.length()];
+        for(int j = 1; j < s.length();j++){
+            for(int i = 0; i < j; i++){
+                boolean innerpalindrome = isPalindrome[i+1][j-1] || j-i <=2;
+                
+                if(s.charAt(i)==s.charAt(j) && innerpalindrome){
+                    isPalindrome[i][j] = true;
+                
+                if(j-i > right-left){
+                    left = i;
+                    right = j;
+                }
+                }
+            }
+        }
+        return s.substring(left, right+1);
     }
-  }
-    
-  return res;
-}
 }
