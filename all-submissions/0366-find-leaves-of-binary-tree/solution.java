87eae3cc2df92badb90e98dda494b721cd859f85
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-11-14 23:48:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 35.1 MB (100.00%)

---
+++ all-submissions/0366-find-leaves-of-binary-tree/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        ArrayList<List<Integer>> leavesList = new ArrayList<>();
+        List<Integer> leaves = new ArrayList<>();
+        
+        while(root!=null){
+            if(isLeaf(root,leaves))
+                root = null;
+            leavesList.add(leaves);
+            leaves = new ArrayList<Integer>();
+            
+        }
+        return leavesList;
+    }
+    public boolean isLeaf(TreeNode node, List<Integer> leaves){
+        if(node.left==null && node.right==null){
+            leaves.add(node.val);
+            return true;
+        }
+        if(node.left!=null){
+            if(isLeaf(node.left,leaves))
+                node.left = null;
+        }
+        if(node.right!=null){
+            if(isLeaf(node.right,leaves))
+                node.right = null;
+        }
+        return false;
+    }
+}
