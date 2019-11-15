592242ba7bd460dd221f475cd61573def6aff16a
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2019-11-15 04:20:46 +0000

    [LeetCode Sync] Runtime - 5 ms (68.00%), Memory - 36.2 MB (100.00%)

---
+++ all-submissions/0139-word-break/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+     if(s==null || s.length() < 1)
+         return false;
+        
+        boolean[] dp = new boolean[s.length()];
+        
+        for(int i = 0; i< s.length(); i++){
+            for(int j = 0; j<=i; j++){
+                String sub = s.substring(j,i+1);
+                if(wordDict.contains(sub) && (j==0||dp[j-1]==true)){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+                  return dp[s.length()-1];
+    }
+}
