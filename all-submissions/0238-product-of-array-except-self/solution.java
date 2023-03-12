76f820ca4c0042d0d89520daae72c2c47039c4eb
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-03-12 06:11:01 +0000

    [LeetCode Sync] Runtime - 1 ms (99.88%), Memory - 51.2 MB (100.00%)

--- all-submissions/0238-product-of-array-except-self/solution.java
+++ all-submissions/0238-product-of-array-except-self/solution.java
@@ -1,23 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] output = new int[nums.length];
-       int left=1; 
-        int right =1;
-        for(int i=0;i<nums.length;i++)
-        {
-            output[i] = 1 ;
+        int length = nums.length;
+        int[] answer = new int[length];
+
+        answer[0] = 1;
+        for (int i= 1; i< length; i++) {
+            answer[i] = nums[i-1] * answer[i-1];
         }
-        for(int i=1;i<nums.length;i++)
-        {
-            left = left* nums[i-1];
-            output[i] =  left;
+        int product = nums[length-1];
+        for (int i = length-2; i >= 0 ;i--){
+            answer[i] = product * answer[i];
+            product = product * nums[i];
         }
-        for(int i=nums.length-2;i>=0;i--)
-        {
-            right = right * nums[i+1];
-            output[i] *= right  ;
-        }
-        
-        return output;
+
+        return answer;
     }
 }
