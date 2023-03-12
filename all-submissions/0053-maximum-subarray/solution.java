402920ce060e5ba02afb31b1f52c1f09b0994140
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-03-12 06:38:48 +0000

    [LeetCode Sync] Runtime - 1 ms (99.97%), Memory - 52.2 MB (100.00%)

--- all-submissions/0053-maximum-subarray/solution.java
+++ all-submissions/0053-maximum-subarray/solution.java
@@ -1,16 +1,12 @@
 class Solution {
-    public int maxSubArray(int[] A) {
-        int n = A.length;
-        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
-        dp[0] = A[0];
-        int max = dp[0];
-        
-        for(int i = 1; i < n; i++){
-            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-            max = Math.max(max, dp[i]);
+    public int maxSubArray(int[] nums) {
+        int curr_subArray = nums[0];
+        int max_subArray = nums[0];
+
+        for (int i =1 ;i< nums.length;i++){
+            curr_subArray= Math.max(nums[i], curr_subArray + nums[i]);
+            max_subArray = Math.max(curr_subArray, max_subArray);
         }
-        
-        return max;
+        return max_subArray;
     }
 }
-   
