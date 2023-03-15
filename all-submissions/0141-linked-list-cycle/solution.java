3bf0ebb7014caf9096e87aac6d071faf7b2cc578
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-03-15 07:54:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.9 MB (99.99%)

--- all-submissions/0141-linked-list-cycle/solution.java
+++ all-submissions/0141-linked-list-cycle/solution.java
@@ -11,17 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null||head.next==null)
-            return false;
-        ListNode slow = head;
+
         ListNode fast = head;
-        while(slow.next!=null&&fast.next!=null&&fast.next.next!=null)
-        {
-            slow = slow.next;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
-            if(slow == fast)
+            slow = slow.next;
+            if (slow == fast){
                 return true;
+            }
         }
         return false;
+        
     }
 }
