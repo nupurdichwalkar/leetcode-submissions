71002fe65ad9677c85ca7951acc0267da2336170
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-04-04 05:03:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.4 MB (100.00%)

--- all-submissions/0070-climbing-stairs/solution.java
+++ all-submissions/0070-climbing-stairs/solution.java
@@ -1,17 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] stairs = new int[2];
-        if(n<=2)
-            return n;
-        stairs[0] = 1;
-        stairs[1] = 2;
-        int ans=0;
-        for(int i = 3; i <= n; i++)
-        {
-            ans = stairs[0]+stairs[1];
-            stairs[0] = stairs[1];
-            stairs[1] = ans;
+        int[] map = new int[n+1];
+        Arrays.fill(map, -1);
+        return climb_Stairs (n, map);
+    }
+
+    public int climb_Stairs(int n, int[] map){
+        if(n < 0) {
+            return 0;
         }
-        return ans;
+        else if (n == 0) {
+            return 1;
+        } else if (map[n] > -1) {
+            return map[n];
+        } else {
+            map[n] =  climb_Stairs(n-1, map) + climb_Stairs(n-2, map);
+            return map[n];
+        }
     }
 }
