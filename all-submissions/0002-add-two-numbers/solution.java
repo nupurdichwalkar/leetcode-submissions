89104549e7ae5a0de462dcfb7aa22c554e55604a
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-04-08 11:17:01 +0000

    [LeetCode Sync] Runtime - 2 ms (5.87%), Memory - 42.9 MB (100.00%)

--- all-submissions/0002-add-two-numbers/solution.java
+++ all-submissions/0002-add-two-numbers/solution.java
@@ -3,30 +3,30 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode curr = new ListNode(-1);
-        ListNode result = curr;
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
         int carry = 0;
-        while (l1 != null || l2 != null)
-    {
-        int x = (l1 != null) ? l1.val : 0;
-        int y = (l2 != null) ? l2.val : 0;
-        int sum = carry + x + y;
-        carry = sum / 10;
-        curr.next = new ListNode(sum % 10);
-        curr = curr.next;
-        if (l1 != null)
-            l1 = l1.next;
-        if (l2 != null) 
-            l2 = l2.next;
-    }
-    if (carry > 0) {
-        curr.next = new ListNode(carry);
-    }
-        return result.next;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+            carry = sum/10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if(l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return head.next;
     }
 }
