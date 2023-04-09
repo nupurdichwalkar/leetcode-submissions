256bb090decafc1a0e3cb174a7e701ab7c1b587b
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-04-09 13:48:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.9 MB (100.00%)

--- all-submissions/0100-same-tree/solution.java
+++ all-submissions/0100-same-tree/solution.java
@@ -4,22 +4,19 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null &&q!=null) return false;
-        if(p!=null && q==null) return false;
-        if(p==null && q==null) return true;
-        
-        
-        boolean left = isSameTree(p.left,q.left);
-        boolean right = isSameTree(p.right,q.right);
-        
-        if(left && right && p.val==q.val)
-            return true;
-        
-        return false;
+        if ( p == null && q == null ) return true;
+        if ( (p != null && q == null) || (p == null && q != null )) return false;
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
