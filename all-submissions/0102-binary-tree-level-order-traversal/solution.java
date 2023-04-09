58b320fc9c283c9160c5a42767c8f5200233046b
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-04-09 17:51:21 +0000

    [LeetCode Sync] Runtime - 1 ms (95.50%), Memory - 43.2 MB (100.00%)

--- all-submissions/0102-binary-tree-level-order-traversal/solution.java
+++ all-submissions/0102-binary-tree-level-order-traversal/solution.java
@@ -4,32 +4,39 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        if (root != null) {
-            q.offer(root);
-        }
-        TreeNode cur;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> subAns = new ArrayList<Integer>();
-            for (int i = 0; i < size; ++i) {        // traverse nodes in the same level
-                cur = q.poll();
-                subAns.add(cur.val);                // visit the root
-                if (cur.left != null) {
-                    q.offer(cur.left);              // push left child to queue if it is not null
-                }
-                if (cur.right != null) {
-                    q.offer(cur.right);             // push right child to queue if it is not null
-                }
+        
+        List<List<Integer>> answer = new ArrayList<List<Integer>>();
+        if (root == null) return answer;
+        // if (root.left == null && root.right == null) return answer;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        int level = 0;
+        queue.add(root);
+        while (!queue.isEmpty()){
+            answer.add(new ArrayList<Integer>());
+            int level_length = queue.size();
+            for (int i=0; i< level_length; i++) {
+                TreeNode node = queue.remove();
+                answer.get(level).add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
             }
-            ans.add(subAns);
+            level++;
         }
-        return ans;
+
+        return answer;
+
+        
     }
 }
