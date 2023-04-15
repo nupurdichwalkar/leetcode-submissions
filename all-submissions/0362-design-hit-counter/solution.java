5fac1c91f0b6e03f135a996c12f42c63c1a6033d
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-04-15 15:16:12 +0000

    [LeetCode Sync] Runtime - 1 ms (93.80%), Memory - 40.5 MB (100.00%)

--- all-submissions/0362-design-hit-counter/solution.java
+++ all-submissions/0362-design-hit-counter/solution.java
@@ -1,25 +1,25 @@
-class HitCounter {
-    private Queue<Integer> hits;
+// class HitCounter {
+//     private Queue<Integer> hits;
 
-    public HitCounter() {
-        this.hits = new LinkedList<Integer>();
-    }
+//     public HitCounter() {
+//         this.hits = new LinkedList<Integer>();
+//     }
     
-    public void hit(int timestamp) {
-        this.hits.add(timestamp);
+//     public void hit(int timestamp) {
+//         this.hits.add(timestamp);
         
-    }
+//     }
     
-    public int getHits(int timestamp) {
-        while(!this.hits.isEmpty()){
-            int diff = timestamp - this.hits.peek();
-            if (diff >= 300) this.hits.remove();
-            else break;
-        }
-        return this.hits.size();
+//     public int getHits(int timestamp) {
+//         while(!this.hits.isEmpty()){
+//             int diff = timestamp - this.hits.peek();
+//             if (diff >= 300) this.hits.remove();
+//             else break;
+//         }
+//         return this.hits.size();
         
-    }
-}
+//     }
+// }
 
 /**
  * Your HitCounter object will be instantiated and called as such:
@@ -27,3 +27,33 @@ class HitCounter {
  * obj.hit(timestamp);
  * int param_2 = obj.getHits(timestamp);
  */
+
+
+
+ class HitCounter {
+     Queue<Integer> hitQueue;
+
+     HitCounter() {
+         hitQueue = new LinkedList<>();
+     }
+
+     public void hit(int timestamp) {
+         hitQueue.add(timestamp);
+     }
+
+     public int getHits(int timestamp) {
+         //remove all queue elements more than 300s old 
+        
+
+        while(!hitQueue.isEmpty()) {
+            int firstElement = hitQueue.peek();
+            if ((timestamp - firstElement) >= 300 ) {
+                hitQueue.remove();
+            } else {
+                break;
+            }
+        }
+
+         return hitQueue.size();
+     }
+ }
