13a25981fece12f04f0308f54cecdc51f2dc7958
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-05-01 20:23:31 +0000

    [LeetCode Sync] Runtime - 1 ms (22.03%), Memory - 49.5 MB (5.49%)

--- all-submissions/0344-reverse-string/solution.java
+++ all-submissions/0344-reverse-string/solution.java
@@ -1,10 +1,10 @@
 class Solution {
-    public String reverseString(String s) {
-        StringBuffer str = new StringBuffer();
-        str.append(s);
-        str.reverse();
-        String A = str.toString();
-        return A;
-        
+    public void reverseString(char[] s) {
+        int len = s.length;
+        for (int i=0 ; i<len/2; i++) {
+            char temp = s[i];
+            s[i] = s[len-i-1];
+            s[len-i-1] = temp;
+        }
     }
 }
