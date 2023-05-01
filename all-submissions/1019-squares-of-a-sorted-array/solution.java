31de1dad9335aca44f8ebb8cf32477bb051158cf
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-05-01 20:36:11 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 44.3 MB (100.00%)

---
+++ all-submissions/1019-squares-of-a-sorted-array/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];        
+        int firstPosIndex = len-1;
+        for (int i =0; i<len; i++) {
+            if(nums[i] >=0) {
+                firstPosIndex = i;
+                break;
+            }
+        }
+        int j = firstPosIndex;
+        int k = firstPosIndex-1;
+        int i=0;
+        while(j<len && k>-1) {
+            int sq1 = nums[j]*nums[j];
+            int sq2 = nums[k]*nums[k];
+            if(sq1 < sq2) {
+                ans[i++] = sq1;
+                j++;
+            }
+            else {
+                ans[i++] = sq2;
+                k--;
+            } 
+        }
+        while(k>-1){
+            ans[i++] = nums[k]*nums[k];
+            k--;
+        }
+        while (j<len){
+            ans[i++] = nums[j]*nums[j];
+            j++;
+        }
+        return ans;
+        
+    }
+}
