04bc40693afcaceec2fd99278b549d3b6803a3bb
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-05-14 21:56:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.7 MB (100.00%)

--- all-submissions/0268-missing-number/solution.java
+++ all-submissions/0268-missing-number/solution.java
@@ -1,23 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // Set<Integer> mySet = new HashSet<>();
-        // for (int num: nums) {
-        //     mySet.add(num);
-        // }
-        // for (int i = 0; i<= nums.length; i++) {
-        //     if (!mySet.contains(i)) {
-        //         return i;
-        //     }
-        // }
-        // return -1;
-        int length = nums.length;
-        int total = (int)(length*(length+1))/2;
-        int arrSum = 0;
+        int len = nums.length;
+        int totalSum = (len*(len+1))/2;
+        int sum = 0;
         for (int num: nums) {
-            arrSum += num;
+            sum +=num;
         }
-        return total-arrSum;
-        
+        return totalSum - sum;
         
     }
 }
