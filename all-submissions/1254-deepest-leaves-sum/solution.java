d3765866cee5b29bd724867fa76ab2eb19ece87d
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-06-19 23:15:21 +0000

    [LeetCode Sync] Runtime - 7 ms (69.96%), Memory - 46.3 MB (100.00%)

---
+++ all-submissions/1254-deepest-leaves-sum/solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    private int findMaxDepth(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(findMaxDepth(node.left), findMaxDepth(node.right)) + 1;
+    }
+    public int deepestLeavesSum(TreeNode root) {
+        if (root == null) return 0;
+        int answer = 0;
+        // int maxDepth = findMaxDepth(root);
+        // int currDepth = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int nodesInCurrLevel = queue.size();
+            answer = 0;
+            for (int i=0; i< nodesInCurrLevel; i++) {
+                TreeNode node = queue.remove();
+                // if (currDepth == maxDepth) {
+                    answer += node.val;
+                // }
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            // currDepth++;
+        }
+        return answer;
+        
+    }
+}
