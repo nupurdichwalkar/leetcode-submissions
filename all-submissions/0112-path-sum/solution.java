99c1f64612048862a3d677a043c3318374772c84
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-06-19 18:56:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.6 MB (99.97%)

--- all-submissions/0112-path-sum/solution.java
+++ all-submissions/0112-path-sum/solution.java
@@ -4,17 +4,22 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
-    if(root == null){
-     return false;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        targetSum -= root.val;
+        if ((root.left == null) && (root.right == null))
+            return (targetSum == 0);
+        return (hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum));
+        
     }
-   if(root.left == null && root.right == null){
-      return (root.val == sum);
-   }
-   return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
-    }      
 }
