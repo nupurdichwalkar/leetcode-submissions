5004c66689384a44ef9f1dc79d0523bd764669b2
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-07-17 05:27:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 39.7 MB (100.00%)

--- all-submissions/0191-number-of-1-bits/solution.java
+++ all-submissions/0191-number-of-1-bits/solution.java
@@ -1,9 +1,16 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        int count = 0;
-        for(int i = 0; i< 32; i++)
-            count += ( (n& (1<<i)) != 0) ? 1 : 0;
-        return count;
+        return Integer.bitCount(n); 
+        // int distance = 0;
+        // while (n != 0) {
+        //   if (n % 2 == 1)
+        //     distance += 1;
+        //   n = n >> 1;
+        // }
+        // return distance;
+        
+        
+
     }
 }
