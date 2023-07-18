186827ff139285910e5783dbd0087f3fa737e941
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-07-18 18:01:32 +0000

    [LeetCode Sync] Runtime - 1 ms (17.80%), Memory - 44.2 MB (99.99%)

--- all-submissions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ all-submissions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -1,18 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if( prices.length == 0){
-            return 0;
+        // if (prices.length == 1) return 0;
+        // int total = 0;
+        // int local_min = Integer.MAX_VALUE;
+        // int local_max = local_min;
+        // for (int i = 0; i< prices.length; i++) {
+        //     if (prices[i] < local_max) {
+        //         total += (local_max - local_min);
+        //         local_min = prices[i];
+        //         local_max = local_min;
+        //     }
+        //     else {
+        //          local_max = prices[i];    
+        //     }
+        // }
+
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
         }
-        int max = 0;
-        int buy = prices[0];
-        for(int i = 1; i < prices.length; i++)
-        {
-            if(buy<prices[i])
-            {
-                max += prices[i]-buy;
-            }
-            buy = prices[i];
-        }
-        return max;
+        return maxprofit;
+        
+        // return total;
+
+        
     }
 }
