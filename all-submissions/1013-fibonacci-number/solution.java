064b3cd8d29565f1d6e76727a036e3f9831dd93c
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-07-19 21:03:08 +0000

    [LeetCode Sync] Runtime - 11 ms (9.28%), Memory - 38.9 MB (100.00%)

--- all-submissions/1013-fibonacci-number/solution.java
+++ all-submissions/1013-fibonacci-number/solution.java
@@ -1,14 +1,9 @@
 class Solution {
-    public int fib(int N) {
-        if(N==0)
-            return 0;
-        int[] F = new int[N+1];
-        F[0]=0;
-        F[1]=1;
-        for(int i =2; i<N+1; i++)
-        {
-            F[i] = F[i-1]+F[i-2];
+    public int fib(int n) {
+        if (n == 1) {
+            return 1;
         }
-        return F[N];
+        if (n <= 0) return 0;
+        return fib(n-1) + fib(n-2);
     }
 }
