4047cc7a8de8b3d036433a63c8c266093fbfb164
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-09-27 06:08:01 +0000

    [LeetCode Sync] Runtime - 4 ms (51.70%), Memory - 54.4 MB (100.00%)

--- all-submissions/0643-maximum-average-subarray-i/solution.java
+++ all-submissions/0643-maximum-average-subarray-i/solution.java
@@ -1,17 +1,15 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-       
-        int left = 0;
-        int right = k-1;
         int sum = 0;
-        for (int i=left; i<=right; i++) {
-            sum += nums[i];
+        int left = 0;
+        for(int i = 0; i < k; i++) {
+            sum  += nums[i];
         }
-        double answer = (double)sum/k;
-        while (right < nums.length-1) {
-            answer = Math.max(answer, (double)sum/k);
-            sum = sum - nums[left++] + nums[++right];
+        double maxAvg = (sum * 1.0) /k;
+        for(int right = k; right < nums.length; right++) {
+            sum += (nums[right] - nums[left++]);
+            maxAvg = Math.max(maxAvg, sum*1.0/k); 
         }
-        return Math.max(answer, (double)sum/k);
+        return maxAvg;
     }
 }
