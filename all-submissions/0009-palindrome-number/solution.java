a33a6ef6eecf75132d66c3ee8ba1a19d644c8a90
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2023-09-29 17:59:22 +0000

    [LeetCode Sync] Runtime - 4 ms (100.00%), Memory - 43.2 MB (100.00%)

--- all-submissions/0009-palindrome-number/solution.java
+++ all-submissions/0009-palindrome-number/solution.java
@@ -2,14 +2,25 @@
 
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        StringBuilder sBuilder = new StringBuilder();
-        sBuilder.append(s);
-        sBuilder.reverse();
-        if(s.equals(sBuilder.toString())) {
-            return true;
+        // String s = String.valueOf(x);
+        // StringBuilder sBuilder = new StringBuilder();
+        // sBuilder.append(s);
+        // sBuilder.reverse();
+        // if(s.equals(sBuilder.toString())) {
+        //     return true;
+        // }
+        // return false;
+        
+        if (x < 0) { return false; }
+        int num = 0;
+        int temp = x;
+        while(temp != 0){
+            int rem = temp%10;
+            num = num*10 + rem;
+            temp = temp/10;
         }
-        return false;
+
+        return x == num;
         
     }
 }
