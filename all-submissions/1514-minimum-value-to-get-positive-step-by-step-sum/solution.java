b0511e7502016685214fb0ca0673f5c4dddf40ab
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-06-10 22:35:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41 MB (100.00%)

--- all-submissions/1514-minimum-value-to-get-positive-step-by-step-sum/solution.java
+++ all-submissions/1514-minimum-value-to-get-positive-step-by-step-sum/solution.java
@@ -1,30 +1,17 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        int startValue = 1;
-        boolean allPositive = false;
-        
-        while(!allPositive) {
-            int[] prefixSum = new int[nums.length];
-            prefixSum[0] = nums[0] + startValue;
-            if (prefixSum[0] < 1) {
-                startValue++;
-                continue;
-            }
-            int i=1;
-            while(i<nums.length) {
-                prefixSum[i] = prefixSum[i-1] + nums[i] ;
-                if(prefixSum[i] < 1){
-                    startValue++;
-                    break;
-                }
-                i++;
-            }
-            if(i == nums.length){
-                allPositive = true;
-            }
+        int[] prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+        int smallestSum = prefixSum[0];
+        for (int i=1;i<nums.length;i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+            smallestSum = Math.min(smallestSum, prefixSum[i]);
         }
-        
-        return startValue;
-        
+
+        if (smallestSum < 0) {
+            return smallestSum * -1 +1;
+        }
+        return 1;
+
     }
 }
