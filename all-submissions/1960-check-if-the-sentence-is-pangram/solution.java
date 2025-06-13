4ac472e189947ca28af39f103142a7f931a57c8e
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-06-13 20:06:57 +0000

    [LeetCode Sync] Runtime - 4 ms (23.57%), Memory - 41.9 MB (99.95%)

--- all-submissions/1960-check-if-the-sentence-is-pangram/solution.java
+++ all-submissions/1960-check-if-the-sentence-is-pangram/solution.java
@@ -1,27 +1,14 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
         Set<Character> set = new HashSet<>();
-        for (char c: sentence.toCharArray()){
-            if(!set.contains(c)) {
-                set.add(c);
+        for (int i=0; i<sentence.length(); i++) {
+            if (!set.contains(sentence.charAt(i))){
+                set.add(sentence.charAt(i));
             }
         }
-        if(set.size() == 26) {
+        if(set.size() == 26){
             return true;
         }
         return false;
-        // int[] charArr = new int[26];
-        // for (int i=0; i<sentence.length(); i++) {
-        //     char c = sentence.charAt(i);
-        //     int index = c - 'a';
-        //     charArr[index]++;
-        // }
-        // for (int num: charArr){
-        //     if(num ==0) {
-        //         return false;
-        //     }
-        // }
-        // return true;
-        
     }
 }
