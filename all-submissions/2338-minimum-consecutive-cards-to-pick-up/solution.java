5f9d8e04db856ab715a14ac03bcc60c8db8f1dea
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-06-16 23:18:47 +0000

    [LeetCode Sync] Runtime - 80 ms (5.19%), Memory - 75 MB (99.65%)

--- all-submissions/2338-minimum-consecutive-cards-to-pick-up/solution.java
+++ all-submissions/2338-minimum-consecutive-cards-to-pick-up/solution.java
@@ -1,31 +1,24 @@
 class Solution {
     public int minimumCardPickup(int[] cards) {
-        Map<Integer, List<Integer>> cardMap = new HashMap<>();
-        int minCount = Integer.MAX_VALUE;
-        for (int i=0; i < cards.length; i++) {
-            if (cardMap.containsKey(cards[i])) {
-                List<Integer> cardMapList = cardMap.get(cards[i]);
-                cardMapList.add(i);
-                cardMap.put(cards[i], cardMapList);
-            } else {
-                int index = i;
-                List<Integer> cardMapList = new ArrayList<Integer>(){
-                    {
-                        add(index);
-                    }
-                };
-                cardMap.put(cards[i], cardMapList);
+        int minCardPick =  Integer.MAX_VALUE;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i=0; i<cards.length;i++) {
+            if(!map.containsKey(cards[i])){
+                map.put(cards[i], new ArrayList<>());
+            }
+            map.get(cards[i]).add(i);
+        }
+        for(int key: map.keySet()){
+            List<Integer> arr = map.get(key);
+            for(int i=0;i<arr.size()-1;i++) {
+                int currMin = arr.get(i+1)- arr.get(i)+1;
+                minCardPick = Math.min(minCardPick, currMin);
             }
         }
-        for (Map.Entry<Integer, List<Integer>> cardEntry: cardMap.entrySet()) {
-            List<Integer> cardIndexList = cardEntry.getValue();
-            if (cardIndexList.size() >= 2) {
-                for (int i = 0; i<cardIndexList.size()-1;i++) {
-                    int diff = cardIndexList.get(i+1) - cardIndexList.get(i) + 1;
-                    minCount = Math.min(minCount, diff);
-                }
-            }
+        if (minCardPick == Integer.MAX_VALUE) {
+            return -1;
         }
-        return minCount == Integer.MAX_VALUE ? -1 : minCount;
+        
+        return minCardPick;
     }
 }
