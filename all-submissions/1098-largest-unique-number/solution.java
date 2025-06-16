7b8b1132d1d113b79eb1baf661846ce7b9ad95bf
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-06-16 21:30:33 +0000

    [LeetCode Sync] Runtime - 5 ms (60.71%), Memory - 42.9 MB (99.74%)

--- all-submissions/1098-largest-unique-number/solution.java
+++ all-submissions/1098-largest-unique-number/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int largestUniqueNumber(int[] nums) {
+    
+        int largestNum = -1;
         Map<Integer, Integer> map = new HashMap<>();
         for (int num: nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        int ans = -1;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if (value == 1) {
-                ans = Math.max(ans, key);
+        for (int key: map.keySet()) {
+            int val = map.get(key);
+            if ( val == 1) {
+                largestNum = Math.max(largestNum, key);
             }
         }
+        return largestNum;
         
-        return ans;
     }
 }
