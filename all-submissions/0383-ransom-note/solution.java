7740685723170fdb3407dd9f980fe9d9149b9fbf
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-06-17 04:32:02 +0000

    [LeetCode Sync] Runtime - 10 ms (44.84%), Memory - 45.1 MB (99.90%)

--- all-submissions/0383-ransom-note/solution.java
+++ all-submissions/0383-ransom-note/solution.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> magCountMap = new HashMap<>();
-        for (char s: magazine.toCharArray()){
-            magCountMap.put(s, magCountMap.getOrDefault(s, 0)+1);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: magazine.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) +1);
         }
-        for (char r : ransomNote.toCharArray()){
-            if(!magCountMap.containsKey(r)){
+        for(char c: ransomNote.toCharArray()) {
+            if(!map.containsKey(c) || map.get(c) == 0) {
                 return false;
             }
-            
-            magCountMap.put(r, magCountMap.get(r)-1);
-            if (magCountMap.get(r) == 0) {
-                magCountMap.remove(r);
-            }
+            map.put(c, map.get(c)-1);
         }
         return true;
         
