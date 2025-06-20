de53a4a0aa44de069c6e374e96cd0cbbec580911
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-06-20 23:51:49 +0000

    [LeetCode Sync] Runtime - 11 ms (10.31%), Memory - 45.7 MB (99.67%)

--- all-submissions/0323-number-of-connected-components-in-an-undirected-graph/solution.java
+++ all-submissions/0323-number-of-connected-components-in-an-undirected-graph/solution.java
@@ -1,36 +1,40 @@
 class Solution {
     Map<Integer, List<Integer>> graph = new HashMap<>();
     Set<Integer> seen = new HashSet<>();
-    int componentCount = 0;
 
     public int countComponents(int n, int[][] edges) {
-        //build a graph
-        for(int[] edge:edges) {
-            int x = edge[0];
-            int y = edge[1];
-            if (!graph.containsKey(x)){
+        int answer = 0;
+        // build graph
+        for (int i = 0; i < edges.length; i++) {
+            int x = edges[i][0];
+            int y = edges[i][1];
+            if (!graph.containsKey(x)) {
                 graph.put(x, new ArrayList<>());
             }
-            if (!graph.containsKey(y)){
+            graph.get(x).add(y);
+            if (!graph.containsKey(y)) {
                 graph.put(y, new ArrayList<>());
             }
-            graph.get(x).add(y);
             graph.get(y).add(x);
         }
-
-        for(int i = 0; i<n ;i++) {
-            if(!seen.contains(i)){
-                componentCount++;
-                dfs(i);
-            }  
+        for (int i = 0; i < n; i++) {
+            if (!graph.containsKey(i)) {
+                graph.put(i, new ArrayList<>());
+            }
         }
-        return componentCount;
+        for (int i = 0; i < n; i++) {
+            if (!seen.contains(i)) {
+                answer++;
+                dfs(i);
+            }
+        }
+        return answer;
     }
 
-    private void dfs(int node){
-        if(graph.containsKey(node)){
-            for(int neighbor: graph.get(node)) {
-                if (!seen.contains(neighbor)){
+    public void dfs(int source) {
+        if (!graph.get(source).isEmpty()) {
+            for (int neighbor : graph.get(source)) {
+                if (!seen.contains(neighbor)) {
                     seen.add(neighbor);
                     dfs(neighbor);
                 }
