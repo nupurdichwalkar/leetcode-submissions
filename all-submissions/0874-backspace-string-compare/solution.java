392fe31385e5854702fa93836d6747a1b1d2403f
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-06-30 20:32:22 +0000

    [LeetCode Sync] Runtime - 1 ms (81.21%), Memory - 41.7 MB (99.95%)

--- all-submissions/0874-backspace-string-compare/solution.java
+++ all-submissions/0874-backspace-string-compare/solution.java
@@ -1,34 +1,31 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> s1 = new Stack<>();
-        Stack<Character> t1 = new Stack<>();
-
+        StringBuilder sb = new StringBuilder();
         for (char c: s.toCharArray()){
-            if (!s1.empty() && c == '#') {
-                s1.pop();
+            if(c != '#') {
+                sb.append(c);
+            } else {
+                if(sb.length() > 0){
+                    sb.deleteCharAt(sb.length() - 1);
+                }
             }
-            else if (c != '#') {
-                s1.push(c);
-            }
-        }
 
+        }
+        StringBuilder tb = new StringBuilder();
         for (char c: t.toCharArray()){
-            if (!t1.empty() && c == '#') {
-                t1.pop();
+            if(c != '#') {
+                tb.append(c);
+            } else {
+                if(tb.length() > 0){
+                    tb.deleteCharAt(tb.length() - 1);
+                }
             }
-            else if (c != '#'){
-                t1.push(c);
-            }
-        }
 
-        while (!s1.empty() && !t1.empty()) {
-            if (s1.pop() != t1.pop()){
-                return false;
-            }
         }
-        if (s1.empty() && t1.empty()) {
-            return true;
-        }
-        return false;
+        String newS = sb.toString();
+        String newT = tb.toString();
+        return newS.equals(newT);
+        
+        
     }
 }
