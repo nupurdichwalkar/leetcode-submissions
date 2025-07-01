487d78535c57dc66b06267696f31df44f89c27ef
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-07-01 18:12:21 +0000

    [LeetCode Sync] Runtime - 436 ms (77.80%), Memory - 59.1 MB (100.00%)

--- all-submissions/2094-remove-stones-to-minimize-the-total/solution.java
+++ all-submissions/2094-remove-stones-to-minimize-the-total/solution.java
@@ -1,20 +1,24 @@
 class Solution {
     public int minStoneSum(int[] piles, int k) {
-        int totalSum = 0;
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int i=0; i<piles.length; i++) {
-            totalSum += piles[i];
-            heap.add(piles[i]);
-        }
-        while(k>0) {
-            int num = heap.remove();
-            int floorVal = (int) num/2;
-            int newNum = num-floorVal;
-            heap.add(newNum);
-            totalSum -= floorVal;
-            k--;
-        }
-        return totalSum;
         
+        PriorityQueue<Integer> pilesQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int pile: piles) {
+            pilesQueue.add(pile);
+        }
+        int operationsCount = 0;
+        for(int i=0; i<k;i++) {
+            if(operationsCount == k) {
+                break;
+            }
+            operationsCount++;
+            int pile = pilesQueue.remove();
+            int newPile = (int)Math.ceil((double)pile/2);
+            pilesQueue.add(newPile);
+        }
+        int answer = 0;
+        for(int pile: pilesQueue) {
+            answer += pile;
+        }
+        return answer;
     }
 }
