a36a65ab83e50e2d8d91de2dd7035c8ba1b151fa
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-07-11 23:53:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.7 MB (99.97%)

--- all-submissions/0070-climbing-stairs/solution.java
+++ all-submissions/0070-climbing-stairs/solution.java
@@ -3,14 +3,15 @@ class Solution {
     public int climbStairs(int n) {
         return dp(n);
     }
-    private int dp(int i){
-        if(i==0) return 0;
-        if (i==1) return 1;
+
+    public int dp(int i){
+        if(i == 0) return 0;
+        if(i==1) return 1;
         if(i==2) return 2;
-        if (memo.containsKey(i)){
+        if(memo.containsKey(i)) {
             return memo.get(i);
         }
-        memo.put(i, dp(i-1) + dp(i-2));
+        memo.put(i, dp(i-1)+dp(i-2));
         return memo.get(i);
     }
 }
