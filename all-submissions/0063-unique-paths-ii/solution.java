e103449a9df329806fd4e79f4fd7b753c0700331
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-07-14 21:30:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (99.93%)

--- all-submissions/0063-unique-paths-ii/solution.java
+++ all-submissions/0063-unique-paths-ii/solution.java
@@ -1,30 +1,48 @@
 class Solution {
+    int m;
+    int n;
+    int[][]memo;
+    int[][] obstacleGrid;
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
-        if(obstacleGrid[0][0]==1)
-            dp[0][0]=0;
-        else
-            dp[0][0]=1;  
-        for(int i = 1; i<obstacleGrid.length;i++){
-            if(obstacleGrid[i][0]==1)
-                dp[i][0]=0;
-            else
-                dp[i][0]=dp[i-1][0];
+        m = obstacleGrid.length;
+        n = obstacleGrid[0].length;
+        this.memo = new int[m][n];
+        for(int i=0; i<m; i++) {
+            Arrays.fill(memo[i], -1);
         }
-        for(int i = 1; i<obstacleGrid[0].length;i++){
-            if(obstacleGrid[0][i]==1)
-                dp[0][i]=0;
-            else
-                dp[0][i]=dp[0][i-1];
+        // If the starting cell has an obstacle, then simply return as there would be
+        // no paths to the destination.
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
         }
-        for(int i = 1; i< obstacleGrid.length; i++){
-            for(int j=1;j<obstacleGrid[0].length;j++){
-                if(obstacleGrid[i][j]==1)
-                dp[i][j]=0;
-            else
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
+
+        if (obstacleGrid[m-1][n-1] == 1) {
+            return 0;
         }
-        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+
+        this.obstacleGrid = obstacleGrid;
+        return getUniquePaths(m-1, n-1);
+    }
+
+    public int getUniquePaths(int row, int col) {
+        if(row + col == 0) {
+            return 1;
+        }
+
+        if(memo[row][col] != -1) {
+            return memo[row][col];
+        }
+        int uniquePathsWithObstacles = 0;
+
+        if(row > 0 && obstacleGrid[row-1][col] != 1 ) {
+            uniquePathsWithObstacles += getUniquePaths(row-1, col);
+        }
+        if(col > 0 && obstacleGrid[row][col-1] != 1 ){
+            uniquePathsWithObstacles += getUniquePaths(row, col-1);
+        } 
+        memo[row][col] = uniquePathsWithObstacles;
+        return uniquePathsWithObstacles;
+
     }
 }
