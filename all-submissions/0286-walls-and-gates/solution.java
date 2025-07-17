baccc6d90aff34dab0cfc327975c1afdd54383b8
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-07-17 22:30:14 +0000

    [LeetCode Sync] Runtime - 8 ms (62.21%), Memory - 48 MB (99.52%)

--- all-submissions/0286-walls-and-gates/solution.java
+++ all-submissions/0286-walls-and-gates/solution.java
@@ -9,37 +9,29 @@ class Solution {
             this.steps = steps;
         }
     }
-
-    int[][] directions = new int[][] {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
-
-
     public void wallsAndGates(int[][] rooms) {
-        if (rooms.length == 0) return;
-        int rows = rooms.length;
-        int cols = rooms[0].length;
+        int[][] directions = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
         Queue<State> queue = new LinkedList<>();
-        boolean[][] seen = new boolean[rows][cols];
-
-        for(int row = 0; row<rows; row++) {
-            for(int col = 0; col < cols ; col++) {
-                if (rooms[row][col] == 0) {
-                    queue.add(new State(row, col,0));
-                    seen[row][col] = true;
+        int m = rooms.length;
+        int n = rooms[0].length;
+        boolean[][] seen = new boolean[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++) {
+                if(rooms[i][j] == 0){
+                    queue.add(new State(i, j, 0));
+                    seen[i][j] = true;
                 }
             }
         }
-
-        while (!queue.isEmpty()) {
-            State state = queue.remove();
+        while(!queue.isEmpty()) {
+            State state = queue.poll();
             int row = state.row;
             int col = state.col;
             int steps = state.steps;
-
-            for(int i =0 ;i< directions.length; i++) {
-                int nextRow = row + directions[i][0];
-                int nextCol = col + directions[i][1];
-                if (isValid(nextRow, nextCol, rooms) && !seen[nextRow][nextCol]){
+            for(int[] direction: directions){
+                int nextRow = row+direction[0];
+                int nextCol = col+direction[1];
+                if(isValid(nextRow, nextCol, rooms) && !seen[nextRow][nextCol]){
                     queue.add(new State(nextRow, nextCol, steps+1));
                     seen[nextRow][nextCol] = true;
                     rooms[nextRow][nextCol] = steps+1;
@@ -49,8 +41,9 @@ class Solution {
         
     }
 
-    public boolean isValid(int nextRow, int nextCol, int[][]rooms) {
-        return (nextRow >= 0 && nextRow < rooms.length && nextCol >= 0 && nextCol<rooms[0].length
-        && rooms[nextRow][nextCol] == Integer.MAX_VALUE);
+    public boolean isValid(int row, int col, int[][] rooms) {
+        return (row>=0 && row<rooms.length 
+        && col>=0 && col<rooms[0].length 
+        && rooms[row][col] == Integer.MAX_VALUE);
     }
 }
