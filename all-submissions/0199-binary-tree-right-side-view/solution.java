dbf3023d8e74d7961e260e659bbe23d37ac3ca71
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-08-04 21:44:49 +0000

    [LeetCode Sync] Runtime - 1 ms (71.39%), Memory - 42.2 MB (99.97%)

--- all-submissions/0199-binary-tree-right-side-view/solution.java
+++ all-submissions/0199-binary-tree-right-side-view/solution.java
@@ -15,33 +15,27 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
+
         Queue<TreeNode> queue = new LinkedList<>();
-        if (root == null) return answer;
+        List<Integer> answer = new ArrayList<>();
+        if(root == null) return answer;
         queue.add(root);
-
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int nodesInCurrentLevel = queue.size();
-            TreeNode answerNode = queue.peek();
-            if (answerNode != null) {
-                answer.add(answerNode.val);
-            }
-            // do some logic here for the current level
-
-            for (int i = 0; i < nodesInCurrentLevel; i++) {
+            for(int i=0;i<nodesInCurrentLevel; i++) {
                 TreeNode node = queue.remove();
-
-
-                if (node.right != null) {
-                    queue.add(node.right);
+                if(i== nodesInCurrentLevel-1) {
+                    answer.add(node.val);
                 }
-
-                // put the next level onto the queue
-                if (node.left != null) {
+                if(node.left != null) {
                     queue.add(node.left);
+                } 
+                if (node.right != null){
+                    queue.add(node.right);
                 }
             }
         }
         return answer;
+        
     }
 }
