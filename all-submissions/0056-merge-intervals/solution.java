d3495499c484bae80ca827f3978668bf5a66397a
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-08-14 23:58:28 +0000

    [LeetCode Sync] Runtime - 7 ms (98.90%), Memory - 46.9 MB (99.99%)

--- all-submissions/0056-merge-intervals/solution.java
+++ all-submissions/0056-merge-intervals/solution.java
@@ -1,32 +1,18 @@
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 class Solution {
-    public List<Interval> merge(List<Interval> intervals) {
-	// sort start&end
-	int n = intervals.size();
-	int[] starts = new int[n];
-	int[] ends = new int[n];
-	for (int i = 0; i < n; i++) {
-		starts[i] = intervals.get(i).start;
-		ends[i] = intervals.get(i).end;
-	}
-	Arrays.sort(starts);
-	Arrays.sort(ends);
-	// loop through
-	List<Interval> res = new ArrayList<Interval>();
-	for (int i = 0, j = 0; i < n; i++) { // j is start of interval.
-		if (i == n - 1 || starts[i + 1] > ends[i]) {
-			res.add(new Interval(starts[j], ends[i]));
-			j = i + 1;
-		}
-	}
-	return res;
-}
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        List<int[]> result = new ArrayList<>();
+        for(int i=0; i< intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            int j = i+1;
+            while( j<intervals.length && end >= intervals[j][0]){
+                end = Math.max(end, intervals[j][1]);
+                j++;
+            }
+            result.add(new int[]{start, end});
+            i=j-1;
+        }
+        return result.toArray(new int[result.size()][]);
+    }
 }
