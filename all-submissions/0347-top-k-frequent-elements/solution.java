da57515dd58c99c6c465cd1f8a36f212eb0f4568
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-08-15 17:58:59 +0000

    [LeetCode Sync] Runtime - 18 ms (13.02%), Memory - 48.3 MB (9.69%)

--- all-submissions/0347-top-k-frequent-elements/solution.java
+++ all-submissions/0347-top-k-frequent-elements/solution.java
@@ -9,17 +9,32 @@ class Solution {
             frequencyMap.put(nums[i], frequencyMap.getOrDefault(nums[i], 0) +1);
         }
 
-        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> frequencyMap.get(n1) - frequencyMap.get(n2));
-
-        for(int key: frequencyMap.keySet()) {
-            heap.add(key);
-            if (heap.size() > k) heap.remove();
-        }
+        Map<Integer, Integer> sortedByValue = frequencyMap.entrySet()
+        .stream()
+        .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed()) // ascending
+        .collect(LinkedHashMap::new,
+                (m, e) -> m.put(e.getKey(), e.getValue()),
+                LinkedHashMap::putAll);
+        int i=0;
         int[] topK = new int[k];
-        for(int i=0; i<k;i++){
-            topK[i] = heap.remove();
+        for(int key: sortedByValue.keySet()){
+            if(i>=k) {
+                break;
+            }
+            topK[i++] = key;
         }
         return topK;
+        // PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> frequencyMap.get(n1) - frequencyMap.get(n2));
+
+        // for(int key: frequencyMap.keySet()) {
+        //     heap.add(key);
+        //     if (heap.size() > k) heap.remove();
+        // }
+        // int[] topK = new int[k];
+        // for(int i=0; i<k;i++){
+        //     topK[i] = heap.remove();
+        // }
+        // return topK;
 
     }
 }
