bdde9d99b7b442891f3f8b44f1f1339650a0fba2
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-08-19 06:28:47 +0000

    [LeetCode Sync] Runtime - 32 ms (79.07%), Memory - 51.5 MB (100.00%)

--- all-submissions/0015-3sum/solution.java
+++ all-submissions/0015-3sum/solution.java
@@ -1,30 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList();
+        int l=0,r=0,sum=0;
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i=0; i< nums.length;i++) {
-            if (i == 0 || nums[i - 1] != nums[i]) {
-                int left = i+1;
-                int right = nums.length -1;
-                while(left < right) {
-                    int sum = nums[left] + nums[right] + nums[i];
-                    if (sum > 0){
-                        right--;
-                    }
-                    else if (sum < 0){
-                        left++;
-                    } 
-                    else {
-                        result.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
-                        while (left < right && nums[left] == nums[left-1]) {
-                            left++;
-                        } 
-                    }
+        for(int i=0; i<nums.length-1;i++){
+            l=i+1;
+            r= nums.length-1;
+            while(l<r){
+                sum = nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                 result.add(Arrays.asList(nums[i],nums[l],nums[r]));     
+                 l++;
+                 r--;
+                 while(l<r && nums[l]==nums[l-1]) l++;
+                 while(l<r && nums[r]==nums[r+1]) r--;
                 }
-            }
-        
+                else if(sum<0){
+                    l++;
+                }
+                else
+                    r--;
+            }            
+            while (i + 1 < nums.length && nums[i + 1] == nums[i]) 
+            i++;
         }
+        
+        
         return result;
-         
     }
 }
