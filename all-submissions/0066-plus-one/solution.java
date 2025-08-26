c05b0090d03f77f3e03114a3dbc0c2e9ea45eb4b
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-08-26 00:24:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.5 MB (99.86%)

--- all-submissions/0066-plus-one/solution.java
+++ all-submissions/0066-plus-one/solution.java
@@ -1,33 +1,21 @@
-import java.io.*;
 class Solution {
     public int[] plusOne(int[] digits) {
-      int carry = 0;
-      int[] temp = new int[digits.length];
-     for(int i = digits.length - 1 ; i>=0; i--)
-     {
-             if(i == digits.length -1)
-                 temp[i] = digits[i] + 1;
-            else
-            {
-                temp[i] = digits[i] + carry;
-                carry = 0;
-            }
-         
-         if( temp[i] > 9 )
-         {
-             carry = 1;
-             temp[i] = 0;
-         }
-               
-     }
-        if(carry == 1)
-        {
-            int[] result = new int[ digits.length+1 ];
-            result[0] = 1;
-            for(int i = 1; i < result.length; i++)
-                result[i] = temp[i-1];
-            return result;
+        List<Integer> aList = new ArrayList<>();
+        int carry = 1;
+        for(int i=digits.length-1; i>=0; i--) {
+            int sum = (digits[i] + carry);
+            carry = sum/10;
+            sum = sum%10;
+            aList.add(sum);
         }
-        return temp;
+        if(carry != 0) {
+            aList.add(carry);
+        }
+        int[] intArray = new int[aList.size()];
+        for (int i = 0; i < aList.size(); i++) {
+            intArray[i] = aList.get(aList.size()-i-1); // Auto-unboxing occurs here
+        }
+        return intArray;
+        
     }
 }
