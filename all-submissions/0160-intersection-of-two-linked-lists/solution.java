4b269eac599ab221faac5904edd5ed82bc6c5a58
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-20 06:49:10 +0000

    [LeetCode Sync] Runtime - 1 ms (99.95%), Memory - 48.9 MB (100.00%)

--- all-submissions/0160-intersection-of-two-linked-lists/solution.java
+++ all-submissions/0160-intersection-of-two-linked-lists/solution.java
@@ -11,50 +11,16 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode currA = headA;
-        ListNode currB = headB;
-        ListNode p1;
-        ListNode p2;
-        int countA = 0;
-        int countB = 0;
-        while(currA!=null)
-        {
-            countA++;
-            currA = currA.next;
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        while(tempA != tempB) {
+            if (tempA == null) 
+                tempA = headB;
+            else tempA = tempA.next;
+            if (tempB == null) tempB = headA;
+            else tempB = tempB.next;
         }
-        while(currB!=null)
-        {
-            countB++;
-            currB = currB.next;
-        }
-        if(countA > countB)
-        {
-            int index = countA - countB;
-            p1 = getNode(headA,index);
-            p2 = headB;
-        }
-        else
-        {
-            int index = countB - countA;
-            p1 = getNode(headB, index);
-            p2 = headA;
-        }
-        while(p1!=p2)
-        {
-            if(p1.next == null || p2.next == null)
-                return null;
-            p1 = p1.next;
-            p2 = p2.next;
-        }
-        return p1;
-    }
-    public ListNode getNode(ListNode head, int index)
-    {
-        ListNode cur = head;
-        for(int i =0; i<index && cur != null; i++)
-        {
-            cur = cur.next;
-        }
-        return cur;
+        return tempA;
     }
 }
