07fa60ddba6010dea842d3db55216ef7c76be1ae
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-22 23:25:10 +0000

    [LeetCode Sync] Runtime - 5 ms (18.49%), Memory - 47.7 MB (100.00%)

--- all-submissions/0160-intersection-of-two-linked-lists/solution.java
+++ all-submissions/0160-intersection-of-two-linked-lists/solution.java
@@ -11,16 +11,22 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
         ListNode tempA = headA;
-        ListNode tempB = headB;
-
-        while(tempA != tempB) {
-            if (tempA == null) 
-                tempA = headB;
-            else tempA = tempA.next;
-            if (tempB == null) tempB = headA;
-            else tempB = tempB.next;
+        Set<ListNode> set = new HashSet<>();
+        while(tempA != null) {
+            set.add(tempA);
+            tempA = tempA.next;
         }
-        return tempA;
+
+        while(headB != null) {
+            if(set.contains(headB)){
+                return headB;
+            }
+            headB = headB.next;
+        }
+
+        return null;
+        
     }
 }
