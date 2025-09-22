7fabc2d1f037b5ad9aa34e5e47b908c1ab6dd848
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-22 23:40:05 +0000

    [LeetCode Sync] Runtime - 1 ms (98.35%), Memory - 46 MB (99.86%)

--- all-submissions/0605-can-place-flowers/solution.java
+++ all-submissions/0605-can-place-flowers/solution.java
@@ -1,34 +1,35 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int mayPlace = 0;
-        if(flowerbed.length == 1 && flowerbed[0] == 0 && n <= 1) {
-            return true;
-        }
-        if(flowerbed.length == 1 && flowerbed[0] == 1) {
-            if(n==0) return true;
+        if(flowerbed == null) return false;
+        if(flowerbed.length == 0 && n >= 1) return false;
+        if(flowerbed.length == 1) {
+            if(flowerbed[0] == 1 && n>=1) return false;
+            if(flowerbed[0] == 1 && n==0) return true;
+            if(flowerbed[0] == 0 && n<=1) return true;
             return false;
         }
-        for (int i=0; i<flowerbed.length; i++) {
-            if (flowerbed[i] == 0) {
-                if (i == 0 && flowerbed[i+1] == 0) {
-                    mayPlace++;
+        if(n == 0) return true;
+        int count = 0;
+        for(int i=0; i<flowerbed.length; i++) {
+            if(i>0 && i<flowerbed.length-1) {
+                if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0) {
+                    count++;
                     flowerbed[i] = 1;
-                } else {
-                    if (i== flowerbed.length-1 && flowerbed[i-1] == 0){
-                        flowerbed[i] = 1;
-                        mayPlace++;
-                    } else 
-                    if(i!= flowerbed.length-1 && i!=0 && flowerbed[i+1] == 0 && flowerbed[i-1] == 0){
-                        flowerbed[i] = 1;
-                        mayPlace++;
-                    }
+                }
+                
+            } else {
+                if(i==0 && flowerbed[i] == 0 && flowerbed[i+1] == 0) {
+                    count++;
+                    flowerbed[i] = 1;
+                }
+                if(i==flowerbed.length-1 && flowerbed[i] == 0 && flowerbed[i-1] == 0) {
+                    count++;
+                    flowerbed[i] = 1;
                 }
             }
         }
-
-        if(mayPlace >= n) {
-            return true;
-        }
+        if (count >= n) return true;
         return false;
+
     }
 }
