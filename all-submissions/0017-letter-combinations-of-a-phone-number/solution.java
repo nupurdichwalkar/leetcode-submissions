6d3ccd4736469538f7d35cb8ccab4a25b35a5726
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-23 23:19:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (99.96%)

--- all-submissions/0017-letter-combinations-of-a-phone-number/solution.java
+++ all-submissions/0017-letter-combinations-of-a-phone-number/solution.java
@@ -1,28 +1,42 @@
 class Solution {
-    private List<String> combinations = new ArrayList<>();
-    private Map<Character, String> letters = Map.of(
-        '2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
-        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+
+    List<String> combinations = new ArrayList<>();
+    Map<Character, String> letters = Map.of('2',"abc",
+        '3',
+        "def",
+        '4',
+        "ghi",
+        '5',
+        "jkl",
+        '6',
+        "mno",
+        '7',
+        "pqrs",
+        '8',
+        "tuv",
+        '9',
+        "wxyz"
+    );
+
+
 
     public List<String> letterCombinations(String digits) {
-        if(digits.length() == 0) {
-            return combinations;
-        }
-        backtrack(0 , digits, new StringBuilder());
+        if(digits.length() == 0) return combinations;
+        StringBuilder sb = new StringBuilder();
+        backtrack(0, sb, digits);
         return combinations;
     }
 
-    public void backtrack(int index, String digits, StringBuilder combination){
-        if(digits.length() == combination.length()){
-            combinations.add(combination.toString());
+    public void backtrack (int index, StringBuilder sb, String digits) {
+        if(sb.length() == digits.length()) {
+            combinations.add(sb.toString());
             return;
         }
-        System.out.print( " " + index);
-        String possibleLetters = letters.get(digits.charAt(index));
-        for(char letter: possibleLetters.toCharArray()){
-            combination.append(letter);
-            backtrack(index+1, digits, combination);
-            combination.deleteCharAt(combination.length()-1);
+        char[] charArray = letters.get(digits.charAt(index)).toCharArray();
+        for(char c: charArray){
+            sb.append(c);
+            backtrack(index+1, sb, digits);
+            sb.deleteCharAt(sb.length() -1);
         }
     }
 }
