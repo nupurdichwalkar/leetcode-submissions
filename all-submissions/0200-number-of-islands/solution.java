e39d91543d7d87d0593df41e805cf0564c4f09dd
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-23 00:28:25 +0000

    [LeetCode Sync] Runtime - 8 ms (10.28%), Memory - 52.4 MB (34.17%)

--- all-submissions/0200-number-of-islands/solution.java
+++ all-submissions/0200-number-of-islands/solution.java
@@ -1,33 +1,48 @@
 class Solution {
 
-    private void dfs(char[][] grid, int row, int col, int rowLength, int colLength) {
-        if (row < 0 || col < 0 || row >= rowLength || col >= colLength || grid[row][col] == '0') {
-            return;
+    class State {
+        int row;
+        int col;
+        State(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
-        grid[row][col] = '0';
-        dfs(grid, row-1, col, rowLength, colLength);
-        dfs(grid, row+1, col, rowLength, colLength);
-        dfs(grid, row, col-1, rowLength, colLength);
-        dfs(grid, row, col+1, rowLength, colLength);
-
     }
+    int[][] directions = new int[][] {{0,1}, {1,0}, {0,-1}, {-1,0}};
+    
 
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        int rowLength = grid.length;
-        int colLength = grid[0].length;
-        int islandCount = 0;
-        for (int i = 0; i< rowLength; i++) {
-            for (int j = 0; j< colLength; j++) {
-                if (grid[i][j] == '1') {
-                    islandCount++;
-                    dfs(grid, i, j, rowLength, colLength);
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] seen = new boolean[m][n];
+        int answer = 0;
+        Queue<State> queue = new LinkedList<>();
+        for(int r=0; r<m;r++) {
+            for (int c=0; c<n; c++) {
+                if(grid[r][c] == '1' && !seen[r][c]) {
+                    answer++;
+                    queue.add(new State(r,c));
+                    seen[r][c] = true;
+                    while(!queue.isEmpty()) {
+                        State state = queue.poll();
+                        int row = state.row;
+                        int col = state.col;
+                        for(int i=0 ;i<directions.length; i++) {
+                            int nextRow = row+directions[i][0];
+                            int nextCol = col+directions[i][1];
+                            if(isValid(nextRow, nextCol, grid) && !seen[nextRow][nextCol]) {
+                                seen[nextRow][nextCol] = true;
+                                queue.add(new State(nextRow, nextCol));
+                            }
+                        }
+                    }
                 }
             }
         }
-        return islandCount;
+        return answer;
+    }
 
+    public boolean isValid(int row, int col, char[][] grid) {
+        return (row >=0 && row < grid.length && col >=0 && col < grid[0].length && grid[row][col] == '1');
     }
 }
