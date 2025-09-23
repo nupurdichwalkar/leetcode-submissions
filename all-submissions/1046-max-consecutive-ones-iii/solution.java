aea3db5d86e55307d2b5217e9e5679862f7b3323
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-23 17:46:50 +0000

    [LeetCode Sync] Runtime - 4 ms (47.75%), Memory - 52.9 MB (7.22%)

--- all-submissions/1046-max-consecutive-ones-iii/solution.java
+++ all-submissions/1046-max-consecutive-ones-iii/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int maxLength=0;
-        int countZero = 0;
-        int left =0;
-        for(int right =0; right<nums.length; right++) {
+        int left = 0;
+        int maxCount =0;
+        int countZeros = 0;
+        for(int right=0; right<nums.length;right++) {
             if(nums[right] == 0) {
-                countZero++;
+                countZeros++;
             }
-            if(countZero > k) {
-                while(left<=right && countZero > k) {
-                    if(nums[left] == 0) {
-                        countZero--;
-                    }
-                    left++;
+            while(countZeros > k && left <= right) {
+                if(nums[left] == 0) {
+                    countZeros--;
                 }
+                left++;
             }
-            maxLength = Math.max(maxLength, right-left+1);
+            maxCount = Math.max(maxCount, right-left+1);
         }
-        return maxLength;
+        return maxCount;
     }
 }
