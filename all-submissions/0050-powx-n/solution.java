7d0cf013a753dacda2d81c69f798710b5e4b2867
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-23 21:23:52 +0000

    [LeetCode Sync] Runtime - 1 ms (10.02%), Memory - 42.4 MB (100.00%)

--- all-submissions/0050-powx-n/solution.java
+++ all-submissions/0050-powx-n/solution.java
@@ -1,6 +1,28 @@
 class Solution {
+
+    Map<Long, Double> powerMap = new HashMap<>(); 
+    double value; 
+
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
-        
+
+        return binaryExp( x, (long) n);
+    }
+
+    private double binaryExp(double x, long n) {
+        if (n == 0) return 1;
+        if(powerMap.containsKey(n)) {
+            return powerMap.get(n);
+        }
+        if (n < 0) 
+          value = 1.0 / binaryExp(x, -n);
+        else if (n % 2 == 1) {
+            value =  x * binaryExp(x * x, (n - 1) / 2);
+        }
+        // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
+        else {
+            value = binaryExp(x * x, n / 2);
+        }
+        powerMap.put(n, value);
+        return value;
     }
 }
