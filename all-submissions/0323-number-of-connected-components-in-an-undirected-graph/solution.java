2f3526cfb5d01c3309b96523e34f1598edc0b140
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-23 23:34:26 +0000

    [LeetCode Sync] Runtime - 8 ms (29.08%), Memory - 45.9 MB (99.29%)

--- all-submissions/0323-number-of-connected-components-in-an-undirected-graph/solution.java
+++ all-submissions/0323-number-of-connected-components-in-an-undirected-graph/solution.java
@@ -1,43 +1,46 @@
 class Solution {
+
     Map<Integer, List<Integer>> graph = new HashMap<>();
-    Set<Integer> seen = new HashSet<>();
 
     public int countComponents(int n, int[][] edges) {
-        int answer = 0;
-        // build graph
-        for (int i = 0; i < edges.length; i++) {
-            int x = edges[i][0];
-            int y = edges[i][1];
-            if (!graph.containsKey(x)) {
+
+        //build the graph
+        for(int[] edge: edges){
+            int x = edge[0];
+            int y = edge[1];
+            if(!graph.containsKey(x)) {
                 graph.put(x, new ArrayList<>());
             }
-            graph.get(x).add(y);
-            if (!graph.containsKey(y)) {
+            if(!graph.containsKey(y)) {
                 graph.put(y, new ArrayList<>());
             }
+            graph.get(x).add(y);
             graph.get(y).add(x);
         }
-        for (int i = 0; i < n; i++) {
-            if (!graph.containsKey(i)) {
-                graph.put(i, new ArrayList<>());
+
+        //DFS
+        int count = 0;
+        boolean[] seen = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!graph.containsKey(i)) {
+                count++;
+                seen[i] = true;
+            } else
+            if(!seen[i]){
+                count++;
+                seen[i] = true;
+                dfs(i, seen);
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (!seen.contains(i)) {
-                answer++;
-                dfs(i);
-            }
-        }
-        return answer;
+        return count;
     }
 
-    public void dfs(int source) {
-        if (!graph.get(source).isEmpty()) {
-            for (int neighbor : graph.get(source)) {
-                if (!seen.contains(neighbor)) {
-                    seen.add(neighbor);
-                    dfs(neighbor);
-                }
+    public void dfs(int index, boolean[] seen) {
+        List<Integer> neighbors = graph.get(index);
+        for(int neighbor: neighbors) {
+            if(!seen[neighbor]) {
+                seen[neighbor] = true;
+                dfs(neighbor, seen);
             }
         }
     }
