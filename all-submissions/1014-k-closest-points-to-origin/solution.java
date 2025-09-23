776e73d40997b0020d0d79eaacc4bc573ceca433
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-23 22:24:23 +0000

    [LeetCode Sync] Runtime - 22 ms (86.36%), Memory - 56.4 MB (31.01%)

--- all-submissions/1014-k-closest-points-to-origin/solution.java
+++ all-submissions/1014-k-closest-points-to-origin/solution.java
@@ -1,17 +1,11 @@
 class Solution {
-    public int[][] kClosest(int[][] points, int K) {
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
-        for (int[] p : points) {
-            pq.offer(p);
-            if (pq.size() > K) {
-                pq.poll();
-            }
-        }
-        int[][] ans = new int[K][2];
-        for(int j = 0; j<K; j++)
-        {
-            ans[j] = pq.poll();
-        }
-        return ans; 
+    public int[][] kClosest(int[][] points, int k) {
+       Arrays.sort(points, (a,b) -> squaredDistance(a) - squaredDistance(b));
+
+       return Arrays.copyOf(points, k);
+    }
+
+    private int squaredDistance(int[] point) {
+        return point[0]*point[0] + point[1]*point[1];
     }
 }
