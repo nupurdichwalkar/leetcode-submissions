76418138e4ad9e265d251dbcb86eb559c646c6cb
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-09-24 18:51:09 +0000

    [LeetCode Sync] Runtime - 1 ms (97.91%), Memory - 41 MB (99.95%)

--- all-submissions/0069-sqrtx/solution.java
+++ all-submissions/0069-sqrtx/solution.java
@@ -1,9 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        int result=(int)Math.abs(Math.sqrt(x));
-        return result;
-        
-        
+        if(x==1) return 1;
+        if(x==0) return 0;
+        long num;
+        int left =2;
+        int right =x/2;
+        while(left<=right) {
+            int mid = left + (right-left) /2;
+            num = (long) mid*mid;
+            if(num == x) return mid;
+            else if(num > x) right = mid-1;
+            else if(num < x) left= mid+1;
+        }
 
+        return right;
     }
 }
