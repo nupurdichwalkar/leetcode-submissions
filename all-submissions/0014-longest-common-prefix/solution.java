e87d4b247e1ff521055232a27fe8d3c45a52d2ab
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-10-08 00:13:27 +0000

    [LeetCode Sync] Runtime - 1 ms (62.69%), Memory - 42.2 MB (99.89%)

--- all-submissions/0014-longest-common-prefix/solution.java
+++ all-submissions/0014-longest-common-prefix/solution.java
@@ -1,27 +1,25 @@
-class Solution {
-   public String longestCommonPrefix(String[] strs) {
-    if (strs == null || strs.length == 0)
-        return "";
-    int minLen = Integer.MAX_VALUE;
-    for (String str : strs)
-        minLen = Math.min(minLen, str.length());
-    int low = 1;
-    int high = minLen;
-    while (low <= high) {
-        int middle = (low + high) / 2;
-        if (isCommonPrefix(strs, middle))
-            low = middle + 1;
-        else
-            high = middle - 1;
-    }
-    return strs[0].substring(0, (low + high) / 2);
-}
 
-private boolean isCommonPrefix(String[] strs, int len){
-    String str1 = strs[0].substring(0,len);
-    for (int i = 1; i < strs.length; i++)
-        if (!strs[i].startsWith(str1))
-            return false;
-    return true;
-}
+
+import static java.lang.Integer.max;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+        int minLength = Integer.MAX_VALUE;
+        for(String s: strs) {
+            minLength = Math.min(minLength, s.length());
+        }
+        for(int i=0;i<minLength;i++) {
+            if(strs[0].length() <= i) return sb.toString();
+            char c = strs[0].charAt(i);
+            for(int j=1; j<strs.length;j++) {
+                if(strs[j].length() <=i) return sb.toString();
+                char d = strs[j].charAt(i);
+                if(c !=d )return sb.toString();
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+        
+    }
 }
