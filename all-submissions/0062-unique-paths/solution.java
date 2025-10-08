041837b35e175d2db597d62c94db946097ff385f
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-10-08 05:08:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (100.00%)

--- all-submissions/0062-unique-paths/solution.java
+++ all-submissions/0062-unique-paths/solution.java
@@ -1,17 +1,20 @@
 class Solution {
+
+    
+
+    public int uniquePathsRecur(int m, int n, int x, int y, int[][]dp) {
+        if(x == m-1 && y == n-1) return 1;
+        if(x >=m || y >=n) return 0;
+        if(dp[x][y]!= 0) return dp[x][y]; 
+        int right = uniquePathsRecur(m, n, x, y+1, dp);
+        int down = uniquePathsRecur(m, n, x+1, y, dp);
+        dp[x][y] = right + down;
+        return dp[x][y];
+    }
+
+
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i = 0; i<m;i++){
-            dp[i][0]=1;
-        }
-        for(int i = 0; i<n;i++){
-            dp[0][i]=1;
-        }
-        for(int i=1; i<m; i++){
-            for(int j =1; j<n;j++){
-                dp[i][j] = dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+        return uniquePathsRecur(m, n, 0, 0, dp);
     }
 }
