67657056204f886ebf266aea9e619b40f9b10b9e
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-10-09 17:27:51 +0000

    [LeetCode Sync] Runtime - 8 ms (95.10%), Memory - 45.9 MB (100.00%)

--- all-submissions/0253-meeting-rooms-ii/solution.java
+++ all-submissions/0253-meeting-rooms-ii/solution.java
@@ -1,21 +1,35 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if(intervals.length==0)
-            return 0;
-        int[] starts = new int[intervals.length],
-        ends = new int[intervals.length];
-        for(int i = 0; i < intervals.length; i++){
-            starts[i] = intervals[i][0];
-            ends[i] = intervals[i][1];
-        }
-        Arrays.sort(starts);
-        Arrays.sort(ends);
-        int ans = 1, ptr = 0;
-        for(int i = 1; i < starts.length; i++){
-            if(starts[i] < ends[ptr]) 
-                ans++;
-            else ptr++;
-        }
-        return ans;
+        
+    // Check for the base case. If there are no intervals, return 0
+    if (intervals.length == 0) {
+      return 0;
     }
+
+    // Min heap
+    PriorityQueue<Integer> allocator =
+        new PriorityQueue<Integer>((a,b) -> a-b);
+
+    // Sort the intervals by start time
+    Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+
+    // Add the first meeting
+    allocator.add(intervals[0][1]);
+
+    // Iterate over remaining intervals
+    for (int i = 1; i < intervals.length; i++) {
+
+      // If the room due to free up the earliest is free, assign that room to this meeting.
+      if (intervals[i][0] >= allocator.peek()) {
+        allocator.poll();
+      }
+
+      // If a new room is to be assigned, then also we add to the heap,
+      // If an old room is allocated, then also we have to add to the heap with updated end time.
+      allocator.add(intervals[i][1]);
+    }
+
+    // The size of the heap tells us the minimum rooms required for all the meetings.
+    return allocator.size();
+  }
 }
