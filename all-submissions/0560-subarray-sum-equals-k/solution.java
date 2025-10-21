950838fce01ef1bdb5e6b92854bffadedbfe1248
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-10-21 06:03:50 +0000

    [LeetCode Sync] Runtime - 1106 ms (23.26%), Memory - 45.9 MB (99.98%)

--- all-submissions/0560-subarray-sum-equals-k/solution.java
+++ all-submissions/0560-subarray-sum-equals-k/solution.java
@@ -1,24 +1,19 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        if (nums==null||nums.length==0)
-            return 0;
-        int left = 0;
-        int right = 0;
-        int count=0;
-        while(left<nums.length)
-        {
-            int sum = 0;
-            while(right<nums.length)
-            {
-                sum+=nums[right];
-                right++;
-            if(sum==k){
-                count++;
-              }
-            }
-                left++;
-                right=left;
+        int[] prefixSum = new int[nums.length+1];
+        prefixSum[0] = 0;
+        for(int i=1; i<= nums.length; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i-1];
         }
-        return count;
+        int answer = 0;
+        for(int i=0; i<nums.length;i++){ // subarraylengths
+            for(int j=i+1; j<=nums.length;j++) {
+                int currSum = prefixSum[j] - prefixSum[i];
+                if (currSum == k) {
+                    answer++;
+                } 
+            }
+        }
+        return answer;
     }
 }
