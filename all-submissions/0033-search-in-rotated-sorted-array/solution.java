54b29a2f5a547a338f953d0d8730e9bd5cd174fd
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-10-21 05:07:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (99.97%)

--- all-submissions/0033-search-in-rotated-sorted-array/solution.java
+++ all-submissions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,37 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==0||nums==null)
-            return -1;
-        return binarySearch(nums,0,nums.length-1,target);
-    } 
-    public int binarySearch(int[] nums, int low, int high, int target){
-        if(low>high)
-            return -1;
-        int mid = (low+high)/2;
-        if(nums[mid]==target)
-            return mid;
-        if(nums[mid]<=nums[low] && nums[mid]<=nums[high])
-        {
-            if(target>nums[mid]&& target<=nums[high]){
-                return binarySearch(nums,mid+1,high,target);
-            }
-            else{
-                return binarySearch(nums,low,mid-1,target);
-            }
+
+       int left =0;
+       int right = nums.length -1;
+       while(left <= right) {
+        int mid = left + (right-left)/2;
+        if (nums[mid] > nums[nums.length-1]){
+            left = mid+1;
+        } else {
+            right = mid-1;
         }
-        else if(nums[mid]>=nums[low] && nums[mid]>=nums[high]){
-            if(target<nums[mid] && target>=nums[low]){
-                return binarySearch(nums,low,mid-1,target);
-            }
-            else{
-                return binarySearch(nums,mid+1,high,target);
-            }
-        }
-        else{
-            if(target<nums[mid])
-                return binarySearch(nums,low,mid-1,target);
-            else
-                return binarySearch(nums,mid+1,high,target);
-        }
+       }
+       int answer = binarySearch(nums, target, 0, left-1);
+       return answer == -1 ? binarySearch(nums, target, left, nums.length-1) : answer;
     }
+
+    public int binarySearch(int[] nums, int target, int left, int right) {
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[mid] > target) {
+                right = mid-1;
+            } else {
+                left = mid +1;
+            }
+        }
+        return -1;
+    }
+
 }
