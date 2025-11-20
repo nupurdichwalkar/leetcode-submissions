5c2867897a81e38256d34e1bedd3235bc2d858d8
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-11-20 06:23:36 +0000

    [LeetCode Sync] Runtime - 538 ms (14.75%), Memory - 48 MB (10.34%)

--- all-submissions/0005-longest-palindromic-substring/solution.java
+++ all-submissions/0005-longest-palindromic-substring/solution.java
@@ -1,25 +1,30 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        if(s==null || s.length()<2){
-            return s;
+
+    public boolean longestPalindromeRecur(String s, int i, int j, boolean[][] dp) {
+        if(i>=j) {
+            return true;
         }
-        int left = 0;
-        int right = 0;
-        boolean[][] isPalindrome = new boolean[s.length()][s.length()];
-        for(int j = 1; j < s.length();j++){
-            for(int i = 0; i < j; i++){
-                boolean innerpalindrome = isPalindrome[i+1][j-1] || j-i <=2;
-                
-                if(s.charAt(i)==s.charAt(j) && innerpalindrome){
-                    isPalindrome[i][j] = true;
-                
-                if(j-i > right-left){
-                    left = i;
-                    right = j;
-                }
+        if(dp[i][j] == true) return true;
+        if(s.charAt(i) == s.charAt(j)) {
+            dp[i][j] = longestPalindromeRecur(s, i+1, j-1, dp);
+        } else {
+            dp[i][j]= false;
+        }
+        return dp[i][j];
+    }
+
+    public String longestPalindrome(String s) {
+        String result = "";
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int i=0; i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(longestPalindromeRecur(s, i, j, dp)){
+                    if(j-i+1 > result.length()){
+                        result = s.substring(i, j+1);
+                    }
                 }
             }
         }
-        return s.substring(left, right+1);
+        return result;
     }
 }
