b73b4c632af71a9094f9422bd822874319ca0f95
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2025-11-20 23:07:55 +0000

    [LeetCode Sync] Runtime - 9 ms (5.27%), Memory - 48 MB (21.92%)

--- all-submissions/0547-number-of-provinces/solution.java
+++ all-submissions/0547-number-of-provinces/solution.java
@@ -1,45 +1,38 @@
 class Solution {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
-    boolean[] seen;
+
+    public void dfs(Map<Integer, List<Integer>> graph, int city, boolean[] seen){
+        for(int neighbor: graph.get(city)){
+            if(!seen[neighbor]) {
+                seen[neighbor] = true;
+                dfs(graph, neighbor, seen);
+            }
+        }
+    }
 
     public int findCircleNum(int[][] isConnected) {
-        // build the graph
-        int n = isConnected.length;
-        for (int i = 0; i < n; i++) {
-            if (!graph.containsKey(i)) {
-                graph.put(i, new ArrayList<>());
-            }
-            for (int j = i + 1; j < n; j++) {
-                if (!graph.containsKey(j)) {
-                    graph.put(j, new ArrayList<>());
-                }
-                if (isConnected[i][j] == 1) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i=0; i<isConnected.length; i++) {
+            for(int j=0; j<isConnected[0].length; j++){
+                if(i!=j && isConnected[i][j] == 1){
+                    graph.putIfAbsent(i, new ArrayList<>());
+                    graph.putIfAbsent(j, new ArrayList<>());
                     graph.get(i).add(j);
                     graph.get(j).add(i);
                 }
             }
         }
+        int provinces =0;
+        boolean[] seen = new boolean[isConnected.length];
 
-        seen = new boolean[n];
-        int ans = 0;        
-        for (int i = 0; i < n; i++) {
-            if (!seen[i]) {
-                // add all nodes of a connected component to the set
-                ans++;
+        for(int i=0;i<isConnected.length; i++) {
+            if(!seen[i]){
+                provinces++;
                 seen[i] = true;
-                dfs(i);
+                if(graph.containsKey(i)){
+                    dfs(graph, i, seen);
+                }
             }
         }
-        
-        return ans;
-    }
-    
-    public void dfs(int node) {
-        for (int neighbor: graph.get(node)) {
-            if (!seen[neighbor]) {
-                seen[neighbor] = true;
-                dfs(neighbor);
-            }
-        }
+        return provinces;
     }
 }
