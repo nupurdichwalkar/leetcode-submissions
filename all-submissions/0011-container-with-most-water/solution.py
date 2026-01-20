d951220ab4f2ea1431d90085e2561b13c997ae8a
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-01-20 19:48:55 +0000

    [LeetCode Sync] Runtime - 468 ms (5.09%), Memory - 30.1 MB (5.48%)

---
+++ all-submissions/0011-container-with-most-water/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left =0
+        right = len(height) -1
+        maxArea = 0
+        if len(height) <= 1:
+            return 0
+        while(left<right):
+            minHeight = min(height[left], height[right])
+            print(left, right, minHeight)
+            
+            currArea = minHeight * (right- left)
+            print(currArea)
+            maxArea = max(maxArea, currArea)
+            if height[left] < height[right]:
+                left+=1
+            else: 
+                right -=1
+        return maxArea
+
