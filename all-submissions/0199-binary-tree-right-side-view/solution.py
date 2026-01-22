a844115cb8036ffcea2707cf080bdade575eb5ad
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-01-22 23:51:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (33.02%)

--- all-submissions/0199-binary-tree-right-side-view/solution.py
+++ all-submissions/0199-binary-tree-right-side-view/solution.py
@@ -4,25 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from collections import deque
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
         res = []
-        def bfs(root):
-            if root is None:
-                return 
-            nonlocal res
-            queue = deque([root])
-            while queue:
-                nodes_in_curr_level = len(queue)
-                for i in range(nodes_in_curr_level):
-                    node = queue.popleft()
-                    if(i == nodes_in_curr_level - 1):
-                        res.append(node.val)
-                    if (node.left):
-                        queue.append(node.left)
-                    if(node.right):
-                        queue.append(node.right)
-        bfs(root)
+        queue = deque([root])
+        # queue.append()
+        while(queue):
+            nodes_in_curr_level = len(queue)
+            res.append(queue[-1].val)
+            for i in range(nodes_in_curr_level):
+                curr_node = queue.popleft()
+                if curr_node.left:
+                    queue.append(curr_node.left)
+                if curr_node.right:
+                    queue.append(curr_node.right)
         return res
