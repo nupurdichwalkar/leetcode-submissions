f97de68761280528dc944f247ba541f59a839efa
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-01-22 06:02:33 +0000

    [LeetCode Sync] Runtime - 3 ms (40.77%), Memory - 20.7 MB (71.90%)

--- all-submissions/1092-maximum-difference-between-node-and-ancestor/solution.py
+++ all-submissions/1092-maximum-difference-between-node-and-ancestor/solution.py
@@ -6,19 +6,15 @@
 #         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        if not root:
+        if root is None:
             return 0
-
-        def helper(node, cur_max, cur_min):
-            # if encounter leaves, return the max-min along the path
-            if not node:
-                return cur_max - cur_min
-            # else, update max and min
-            # and return the max of left and right subtrees
-            cur_max = max(cur_max, node.val)
-            cur_min = min(cur_min, node.val)
-            left = helper(node.left, cur_max, cur_min)
-            right = helper(node.right, cur_max, cur_min)
+        def dfs_helper(node, curr_max, curr_min):
+            if node is None:
+                return curr_max - curr_min
+            curr_max = max(node.val, curr_max)
+            curr_min = min(node.val, curr_min)
+                    
+            left = dfs_helper(node.left, curr_max, curr_min)
+            right = dfs_helper(node.right, curr_max, curr_min)
             return max(left, right)
-
-        return helper(root, root.val, root.val)
+        return dfs_helper(root, root.val, root.val)
