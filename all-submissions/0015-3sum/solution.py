3b23e56fac076a57a94ebfbe7d54555ae715ad0b
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-02-02 21:14:09 +0000

    [LeetCode Sync] Runtime - 663 ms (35.25%), Memory - 22.5 MB (18.41%)

--- all-submissions/0015-3sum/solution.py
+++ all-submissions/0015-3sum/solution.py
@@ -1,29 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        if nums is None or len(nums) < 3: return []
         nums.sort()
-        n = len(nums)
-        for i in range(n-2):
-            if i > 0 and nums[i] == nums[i-1]:
+        answer = []
+        for i in range(len(nums)-2):
+            if (i>0 and nums[i] == nums[i-1]): 
                 continue
-            j = i+1
-            k = n-1
-            while j < k: 
-                triplet_sum = nums[i] + nums[j] + nums[k]
-                if triplet_sum == 0:
-                    res.append([nums[i], nums[j], nums[k]])
+            j = i +1 
+            k = len(nums)-1
+            while(j < k):
+                sum = nums[i] + nums[j] + nums[k] 
+                if sum == 0:
+                    answer.append([nums[i],nums[j],nums[k]])
                     j+=1
                     k-=1
-                    while j<k and nums[j] == nums[j-1]:
-                        j+=1
-                    while j<k and nums[k] == nums[k+1]:
-                        k-= 1
-                else:
-                    if triplet_sum > 0:
-                        k-=1
-                    else:
-                        j+=1
+                    while(0 < j < len(nums) and nums[j] == nums[j-1]): j+=1
+                    while(k<len(nums)-1 and k>=0 and nums[k] == nums[k+1]): k -=1
+                elif sum > 0:
+                    k-=1
+                else: 
+                    j+=1
+        return answer
 
-            print(nums[i], nums[i+1])
-            
-        return res
