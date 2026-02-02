5a5970fe4c2d7d65e17ac482d0abd52d1188c3a5
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-02-02 22:37:27 +0000

    [LeetCode Sync] Runtime - 286 ms (10.03%), Memory - 31.1 MB (9.33%)

--- all-submissions/0200-number-of-islands/solution.py
+++ all-submissions/0200-number-of-islands/solution.py
@@ -1,26 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def isValid(row, col):
-            return row >= 0 and row < m and col >= 0 and col < n and grid[row][col] == "1"
- 
+            return 0<=row<len(grid) and 0<=col<len(grid[0]) and grid[row][col] == "1"
+	
         def dfs(row, col):
-            for dx, dy in directions:
-                nextRow = row + dx
-                nextCol = col + dy
-                if isValid(nextRow, nextCol) and \
-                    (nextRow, nextCol) not in seen :
-                    seen.add((nextRow, nextCol))
-                    dfs(nextRow, nextCol)
+            if (isValid(row, col) and (row, col) not in seen):			
+                seen.add((row, col))
+                dfs(row+1, col)
+                dfs(row-1, col)
+                dfs(row, col+1)
+                dfs(row, col-1)
 
-        directions = [(1,0), (0,1), (-1, 0), (0, -1)]
+
         seen = set()
         ans = 0
-        m =len(grid)
-        n = len(grid[0])
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == "1" and (row,col) not in seen:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1" and (i,j) not in seen:
+                    dfs(i, j)
                     ans +=1
-                    seen.add((row, col))
-                    dfs(row, col)
         return ans
+
