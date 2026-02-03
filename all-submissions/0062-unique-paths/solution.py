843f96e3c199a498cf67577587d6efe065d5afce
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-02-03 20:48:22 +0000

    [LeetCode Sync] Runtime - 2 ms (24.00%), Memory - 20.2 MB (5.43%)

---
+++ all-submissions/0062-unique-paths/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        dp ={}
+
+        def dfs(row, col):
+            if row >= m or col >= n:
+                return 0
+            if row == m-1 and col == n-1:
+                return 1
+            if (row, col) in dp:
+                return dp[(row, col)]
+            dp[(row, col)] = dfs(row + 1, col) + dfs(row, col+1)
+            return dp[(row, col)]
+        
+        return dfs(0, 0)
