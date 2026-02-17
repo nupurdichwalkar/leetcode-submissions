503d54f98fecb2172ee4d4c0f55d8d86e60a0d7e
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-02-17 20:43:25 +0000

    [LeetCode Sync] Runtime - 41 ms (70.13%), Memory - 28.6 MB (42.77%)

---
+++ all-submissions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        buyPrice = float("inf")
+        for i in range(len(prices)):
+            if prices[i] <  buyPrice:
+                buyPrice = prices[i]
+            elif prices[i]-buyPrice > max_profit:
+                max_profit = prices[i]-buyPrice
+        return max_profit
