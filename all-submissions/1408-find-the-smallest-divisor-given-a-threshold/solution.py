a25434ab880dc61ee64dbbcc8706e99cb086ec52
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-02-25 01:25:46 +0000

    [LeetCode Sync] Runtime - 129 ms (64.00%), Memory - 24.6 MB (13.56%)

---
+++ all-submissions/1408-find-the-smallest-divisor-given-a-threshold/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        def check(divisor):
+            sum = 0
+            for num in nums:
+                sum += ceil(num/divisor)
+            return sum <= threshold
+        
+        left =1
+        right = max(nums)
+        while left <= right:
+            mid = (left+right)//2
+            if check(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
