125a94369536f7ce75a6024677749d957d21e3fc
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-02-25 18:36:49 +0000

    [LeetCode Sync] Runtime - 153 ms (14.90%), Memory - 61.4 MB (26.93%)

--- all-submissions/0077-combinations/solution.py
+++ all-submissions/0077-combinations/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res = []
-        def backtrack(curr, seen, num):
-            if (len(curr) == k):
-                res.append(curr.copy())
+        res =[] 
+        def backtrack(num, arr):
+            if len(arr) == k:
+                res.append(arr[:])
                 return
             for i in range(num, n+1):
-                if i not in seen:
-                    seen.add(i)
-                    curr.append(i)
-                    backtrack(curr, seen, i+1)
-                    seen.remove(i)
-                    curr.pop()
-        backtrack([], set(), 1)
+                arr.append(i)
+                backtrack(i+1, arr)
+                arr.remove(i)
+
+        backtrack(1, [])
         return res
+
