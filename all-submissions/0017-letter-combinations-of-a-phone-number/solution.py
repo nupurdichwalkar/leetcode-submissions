6e29fa56f0388888f3d89dd782161629470a953a
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-02-25 21:40:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (9.89%)

---
+++ all-submissions/0017-letter-combinations-of-a-phone-number/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        map = {"2": "abc", 
+                "3":"def", 
+                "4": "ghi",
+                "5":"jkl",
+                "6": "mno", 
+                "7": "pqrs",
+                "8":"tuv",
+                "9":"wxyz"}
+
+        res = []
+        def backtrack(curr_str, curr_index):
+            if len(curr_str) == len(digits):
+                res.append("".join(curr_str))
+                return
+            num = digits[curr_index]
+            for c in map[num]:
+                curr_str += c
+                backtrack(curr_str, curr_index+1)
+                curr_str.pop()
+        backtrack([], 0)
+        return res
+
