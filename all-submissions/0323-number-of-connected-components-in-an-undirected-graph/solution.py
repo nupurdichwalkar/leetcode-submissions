a89a9e8164bda2d77638c510d3b4bac07c11b414
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-03-09 00:06:00 +0000

    [LeetCode Sync] Runtime - 1 ms (93.64%), Memory - 20.4 MB (58.76%)

--- all-submissions/0323-number-of-connected-components-in-an-undirected-graph/solution.py
+++ all-submissions/0323-number-of-connected-components-in-an-undirected-graph/solution.py
@@ -1,22 +1,31 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        def dfs(edge):
-            for neighbor in graph[edge]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    dfs(neighbor)
-
-        # create graph
-        graph = defaultdict(list)
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
+        root = [i for i in range(n)]
+        rank = [1] * n
+        def find(x):
+            if x != root[x]:
+                root[x] = find(root[x])
+            return root[x]
         
-        visited = set()
-        count = 0
-        for edge in range(n) :
-            if edge not in visited:
-                visited.add(edge)
-                count +=1
-                dfs(edge)
+        def union(x,y):
+            rootX,rootY = find(x), find(y)
+            if rootX == rootY:
+                return 0
+            if rank[rootX] > rank[rootY]:
+                root[rootY] = rootX
+            elif rank[rootY] > rank[rootX]:
+                root[rootX] = rootY
+            else:
+                root[rootY] = rootX
+                rank[rootX] +=1
+            return 1
+
+        # def isConnected(x,y):
+        #     return find(x) == find(y)
+
+        count = n
+        for i, j in edges:
+            count -= union(i, j)
+
         return count
+            
