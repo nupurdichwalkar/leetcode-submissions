35412874b0fef6475fcd37276f775c69634a4770
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-03-15 04:14:12 +0000

    [LeetCode Sync] Runtime - 11 ms (37.05%), Memory - 19.2 MB (54.34%)

--- all-submissions/0009-palindrome-number/solution.py
+++ all-submissions/0009-palindrome-number/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        s = str(x)
-        my_list = [int(digit) for digit in s]
-        my_new_list = my_list[::-1]
-        if (my_list) == my_new_list:
-            return True
-        return False
+        return str(x) == str(x)[::-1]
+        # my_list = [int(digit) for digit in s]
+        # my_new_list = my_list[::-1]
+        # if (my_list) == my_new_list:
+        #     return True
+        # return False
 
