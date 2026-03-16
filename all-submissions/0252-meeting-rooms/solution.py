e30c659ddc42143986e946d9a2831e616dd36df8
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-03-16 19:01:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.3 MB (40.31%)

--- all-submissions/0252-meeting-rooms/solution.py
+++ all-submissions/0252-meeting-rooms/solution.py
@@ -2,8 +2,8 @@ class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         if not intervals:
             return True
-        if len(intervals) == 0:
-            return True
+        # if len(intervals) == 0:
+        #     return True
         intervals.sort(key = lambda x:x[0])
         end_time = intervals[0][1]
         for interval in intervals[1:]:
