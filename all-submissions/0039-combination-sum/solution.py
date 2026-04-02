b39fbb7b03c40f455d98b830f2d8a5f00f4c3f3a
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-02 00:34:39 +0000

    [LeetCode Sync] Runtime - 9 ms (52.75%), Memory - 19.6 MB (64.89%)

--- all-submissions/0039-combination-sum/solution.py
+++ all-submissions/0039-combination-sum/solution.py
@@ -1,17 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
         res = []
-        def backtrack(currSum, currIndex, ans):
-            if currSum == target:
-                res.append(ans.copy())
-                return 
-            if currSum > target:
+        
+        def backtrack(curr_sum, index, curr_res):
+            if curr_sum == target:
+                res.append(curr_res[::])
                 return
-            for i in range(currIndex, len(candidates)):
-                ans.append(candidates[i])
-                backtrack(currSum + candidates[i], i, ans)
-                ans.pop()
-        backtrack(0,0, [])
+            if curr_sum > target:
+                return
+            for i in range(index, len(candidates)):
+                curr_res.append(candidates[i])
+                backtrack(curr_sum + candidates[i], i, curr_res)
+                curr_res.remove(candidates[i])
+        
+        backtrack(0, 0, [])
+        
         return res
-
+            
