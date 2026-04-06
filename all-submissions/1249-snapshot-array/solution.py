646c7ec210713c13e0d8cef950fd16560873f3a8
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-06 23:20:13 +0000

    [LeetCode Sync] Runtime - 153 ms (77.21%), Memory - 50.3 MB (18.39%)

---
+++ all-submissions/1249-snapshot-array/solution.py
@@ -0,0 +1,23 @@
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.snap_id = 0
+        self.arr = [[[0, 0]] for _ in range(length)]
+
+    def set(self, index: int, val: int) -> None:
+        self.arr[index].append([self.snap_id, val])
+
+    def snap(self) -> int:
+        self.snap_id +=1
+        return self.snap_id-1
+
+    def get(self, index: int, snap_id: int) -> int:
+        snap_index = bisect.bisect_right(self.arr[index], [snap_id, 10**9])
+        return self.arr[index][snap_index - 1][1]
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
