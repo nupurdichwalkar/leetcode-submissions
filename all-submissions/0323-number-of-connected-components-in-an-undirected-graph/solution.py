3174a067327bcdd0c671bc07aac73e9adfd036d6
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-10 05:07:19 +0000

    [LeetCode Sync] Runtime - 2 ms (91.75%), Memory - 21.5 MB (9.30%)

--- all-submissions/0323-number-of-connected-components-in-an-undirected-graph/solution.py
+++ all-submissions/0323-number-of-connected-components-in-an-undirected-graph/solution.py
@@ -1,31 +1,21 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        root = [i for i in range(n)]
-        rank = [1] * n
-        def find(x):
-            if x != root[x]:
-                root[x] = find(root[x])
-            return root[x]
-        
-        def union(x,y):
-            rootX,rootY = find(x), find(y)
-            if rootX == rootY:
-                return 0
-            if rank[rootX] > rank[rootY]:
-                root[rootY] = rootX
-            elif rank[rootY] > rank[rootX]:
-                root[rootX] = rootY
-            else:
-                root[rootY] = rootX
-                rank[rootX] +=1
-            return 1
+        adj = defaultdict(list)
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
 
-        # def isConnected(x,y):
-        #     return find(x) == find(y)
-
-        count = n
-        for i, j in edges:
-            count -= union(i, j)
+        def dfs(i):
+            for edge in adj[i]:
+                if edge not in visited:
+                    visited.add(edge)
+                    dfs(edge)
+        count = 0
+        visited = set()
+        for i in range(n):
+            if i not in visited:
+                count +=1
+                visited.add(i)
+                dfs(i)
 
         return count
-            
