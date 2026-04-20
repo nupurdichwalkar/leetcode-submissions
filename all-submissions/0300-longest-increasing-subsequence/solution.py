bc05880d7e81c49cb6355458a6f63bd5d9a22036
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-20 18:48:42 +0000

    [LeetCode Sync] Runtime - 1275 ms (31.52%), Memory - 19.5 MB (60.15%)

--- all-submissions/0300-longest-increasing-subsequence/solution.py
+++ all-submissions/0300-longest-increasing-subsequence/solution.py
@@ -1,12 +1,9 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1]* len(nums)
-        def dfs(currIndex):
-            if currIndex == len(nums):
-                return
-            for i in range(currIndex): 
-                if nums[i] < nums[currIndex]:
-                    dp[currIndex] = max(dp[currIndex], dp[i]+1)
-            dfs(currIndex+1)
-        dfs(0)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+
         return max(dp)
