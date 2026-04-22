ec481577c7f3cd27c66946c59f6d0462e686d3e4
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-22 23:35:55 +0000

    [LeetCode Sync] Runtime - 11 ms (92.38%), Memory - 20.2 MB (86.19%)

--- all-submissions/0055-jump-game/solution.py
+++ all-submissions/0055-jump-game/solution.py
@@ -1,19 +1,16 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = [-1]*len(nums)
-        dp[-1] = 1
-        def canJumpFromIndex(index):
-            if dp[index] != -1:
-                return dp[index] == 1
-            if index == len(nums):
-                return True
-            if index > len(nums):
+        end = len(nums)
+        i = 0
+
+        for num in nums:
+            if i < 0:
                 return False
-            furthestJump = min(nums[index]+1, len(nums))
-            for i in range(1, furthestJump):
-                if canJumpFromIndex(index+i):
-                    dp[i] = 1
-                    return True
-            dp[index] = 0
-            return False
-        return canJumpFromIndex(0)
+            
+            elif num > i:
+                i = num
+            
+            i-=1
+        
+        return True
+        
