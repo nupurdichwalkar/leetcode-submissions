a5cb3c5845ba50c316c79000ab3ae294c6a32522
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-23 05:29:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.4 MB (74.37%)

--- all-submissions/0210-course-schedule-ii/solution.py
+++ all-submissions/0210-course-schedule-ii/solution.py
@@ -1,23 +1,28 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
         indegree = defaultdict(int)
-        for dest, src in prerequisites:
-            graph[src].append(dest)
-            indegree[dest] +=1
-        
-        queue = collections.deque()
-        for k in range(numCourses):
-            if k not in indegree:
-                queue.append(k)
-        res = []
-        while queue:
-            vertex = queue.popleft()
-            res.append(vertex)
-            if vertex in graph:
-                for neighbor in graph[vertex]:
-                    indegree[neighbor] -=1
-                    if indegree[neighbor] == 0:
-                        queue.append(neighbor)
+        adj_list = defaultdict(list)
 
-        return res if len(res) == numCourses else []
+        for dest, source in prerequisites :
+            indegree[dest] +=1
+            adj_list[source].append(dest)
+
+        queue = deque()
+
+        for i in range(numCourses):
+            if i not in indegree:
+                queue.append(i)
+
+        ans = []
+
+        while queue:
+            course = queue.popleft()
+            ans.append(course)
+            for dep_course in adj_list[course]:
+                indegree[dep_course] -=1
+                if indegree[dep_course] == 0:
+                    queue.append(dep_course)
+
+        return ans if len(ans) == numCourses else []
+
+        
