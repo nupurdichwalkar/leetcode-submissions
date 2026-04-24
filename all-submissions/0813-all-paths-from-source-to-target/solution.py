33b3671e4b9f08e6da9b52391add5a58c29a5a20
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-24 22:16:20 +0000

    [LeetCode Sync] Runtime - 3 ms (93.57%), Memory - 20.6 MB (25.83%)

--- all-submissions/0813-all-paths-from-source-to-target/solution.py
+++ all-submissions/0813-all-paths-from-source-to-target/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         res = []
-        n = len(graph)
-        def dfs(curr, path):
-            if curr == n-1:
-                res.append(path[:])
-                return
-            for i in graph[curr]:
-                path.append(i)
-                dfs(i, path)
-                path.pop()
+        def dfs(n, ans):
+            if n == len(graph)-1:
+                res.append(ans[:])
+            for i in graph[n]:
+                ans.append(i)
+                dfs(i, ans)
+                ans.pop()
+
         dfs(0, [0])
-        return res	
+        return res
