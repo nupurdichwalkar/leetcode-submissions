813cbd269ae5ae714f2ae554e5d558753dd59483
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-24 23:46:18 +0000

    [LeetCode Sync] Runtime - 47 ms (23.78%), Memory - 31.5 MB (20.49%)

--- all-submissions/0053-maximum-subarray/solution.py
+++ all-submissions/0053-maximum-subarray/solution.py
@@ -3,13 +3,14 @@ class Solution:
         left = 0
         right = 1
         if not nums: return 0
-        max_sum = nums[0]
-        curr_sum = nums[0]
-        while right < len(nums):
-            curr_sum += nums[right]
-            if curr_sum < nums[right]:
-                curr_sum = nums[right]
+        currSum = nums[0]
+        maxSum = nums[0]
+        while(right < len(nums)):
+            currSum += nums[right]
+            if currSum < nums[right]:
+                currSum = nums[right]
                 left = right
-            max_sum = max(max_sum, curr_sum)
-            right +=1
-        return max_sum
+            maxSum = max(maxSum, currSum)
+            right+=1
+        
+        return maxSum
