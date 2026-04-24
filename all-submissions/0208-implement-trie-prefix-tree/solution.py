9abe0c82909e650cb55bdda15e7cb40e78c4a79b
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-24 17:45:18 +0000

    [LeetCode Sync] Runtime - 54 ms (19.22%), Memory - 36.9 MB (7.64%)

---
+++ all-submissions/0208-implement-trie-prefix-tree/solution.py
@@ -0,0 +1,58 @@
+class TrieNode:
+
+    def __init__(self):
+        self.links = [None]*26
+        self.is_end = False
+
+    def contains_key(self, ch:str) -> bool:
+        return self.links[ord(ch) - ord('a')] is not None
+
+    def get(self, ch:str) -> TrieNode:
+        return self.links[ord(ch) - ord('a')] 
+
+    def put(self, ch:str, node: TrieNode) -> None:
+        self.links[ord(ch) - ord('a')]  = node
+
+    def set_end(self) -> None:
+        self.is_end = True
+
+    def is_end_(self)-> bool:
+        return self.is_end
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if not node.contains_key(ch):
+                node.put(ch, TrieNode())
+            node = node.get(ch)
+        node.set_end()
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for ch in word:
+            if not node.contains_key(ch):
+                return False
+            node = node.get(ch)
+        return node.is_end_()
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for ch in prefix:
+            if not node.contains_key(ch):
+                return False
+            node = node.get(ch)
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
