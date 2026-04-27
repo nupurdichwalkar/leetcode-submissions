bdd0f2477f50463372ea343173b417d1b9010d32
Nupur Dichwalkar <nupur.dichwalkar@gmail.com>
2026-04-27 21:10:06 +0000

    [LeetCode Sync] Runtime - 48 ms (57.93%), Memory - 28.6 MB (76.48%)

--- all-submissions/0121-best-time-to-buy-and-sell-stock/solution.py
+++ all-submissions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_profit = 0
-        buyPrice = float("inf")
+        min_price = float("inf")
         for i in range(len(prices)):
-            if prices[i] <  buyPrice:
-                buyPrice = prices[i]
+            if prices[i] < min_price:
+                min_price = prices[i]
             else:
-                max_profit = max(max_profit, prices[i]-buyPrice)
+                max_profit = max(max_profit, prices[i]-min_price)
         return max_profit
+
